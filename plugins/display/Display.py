74ee8a239a197922413b5cd817311f85f5af0e86
Daniel Coderre <coderdj@gmail.com>
2014-07-22 22:31:11 +0200

    Added extremely simple pmt pattern display using svg circles (d3.js)

--- plugins/display/Display.py
+++ plugins/display/Display.py
@@ -14,8 +14,9 @@ class DisplayPage(object):
     """ Cherrypy website for displaying event data. """
     def __init__(self, config):                
         self.event=None
-        self.topArrayMap = config['topArrayMap']
+        self.top_array_map = config['topArrayMap']
         self.rdy=False
+        self.my_directory = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
     def SetEvent(self,event):
         self.rdy=False
@@ -33,18 +34,36 @@ class DisplayPage(object):
     def index(self):
         if self.event == None:
             return """<html><head></head><body><h4>No event</h4></body></html>"""  
-        direc = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        return open(os.path.join(direc,'public','html','index.html'))
+        return open(os.path.join(self.my_directory,'public','html','index.html'))
     
     @cherrypy.expose
     def pmtpattern(self):
-        return """<html>
-        <head></head>
-        <body>
-           <a href="/">Home</a>
-        </body>
-        </html>"""
-    
+        return open(os.path.join(self.my_directory,'public','html','pmtpattern.html'))
+
+    @cherrypy.expose
+    def get_pmtpattern(self):
+        geo = []
+        total = np.sum(self.event['processed_waveforms']['top_and_bottom'])
+        for i in range(0, len(self.top_array_map)):
+            size = 0
+            if i not in self.event['channel_waveforms'].keys():
+                size=0
+            else:
+                row = np.sum(self.event['channel_waveforms'][i])
+                size = (int)(row/(total/248) * 10)
+                if size>10:
+                    size=10
+            color = "red"
+            if size == 0:
+                color = "blue"         
+                size = 1
+            geo.append({"x_axis": self.top_array_map[i]['x'], 
+                        "y_axis": self.top_array_map[i]['y'],
+                        "radius": size,
+                        "color": color})
+        ret = {'geometry': geo}
+        return json.dumps(ret)
+
     @cherrypy.expose
     def get_waveform(self):
         points = []
@@ -53,7 +72,7 @@ class DisplayPage(object):
         #now get peaks
         s1Rank = s2Rank = 0
         peaks = []
-        for peak in sorted(self.event['peaks'], key=lambda x: x['top_and_bottom']['area']):
+        for peak in sorted(self.event['peaks'], key=lambda x: 1/x['top_and_bottom']['area']):
             if peak['rejected']:
                 continue
             x = peak['top_and_bottom']['position_of_max_in_waveform']
@@ -66,7 +85,7 @@ class DisplayPage(object):
                 
             else:
                 s1Rank+=1
-            peaks.append( [ptype, rank, str(x), '%2f' % peak['top_and_bottom']['area'], '%2f' % peak['left'], '%2f' % peak['right']])
+            peaks.append( [ptype, rank, str(x), '%2f' % peak['top_and_bottom']['area'], '%i' % peak['left'], '%i' % peak['right']])
                 
         ret = {'waveform': points, 'peaks': peaks}
         return json.dumps(ret)
