0a7b540c884f174338ee5a73407e65ef5866fc95
Jelle Aalbers <jelleaalbers@gmail.com>
2014-07-31 21:41:57 +0200

    Plotting changes to aid debugging
    More information in plot, less pretty though... may want to make
    separate plot classes at some point?

--- plugins/io/Plotting.py
+++ plugins/io/Plotting.py
@@ -15,26 +15,35 @@ class PlottingWaveform(plugin.OutputPlugin):
         ax = fig.add_subplot(111)
 
         # Plot all peaks
+        max_y = max([p.peak_dict['height'] for p in event.S2s() + event.S1s()])
+
         for peak in event.S2s() + event.S1s():
 
-            x = peak.time_in_waveform()
-            y = event.summed_waveform()[x]
+            x = peak.peak_dict['index_of_max_in_waveform']
+            y = peak.peak_dict['height']
 
             plt.hlines(y, *peak.bounds())
-            ax.annotate('%s:%s' % (peak.peak_dict['peak_type'], int(peak.area())),
+            ax.annotate('%s:%s' % (peak.__class__.__name__, int(peak.area())),
                         xy=(x, y),
-                        xytext=(x, y + 100 + random.random() * 100),
+                        xytext=(x, y + (max_y-y)*(0.05+0.2*random.random())),
                         arrowprops=dict(arrowstyle="fancy",
                                         fc="0.6", ec="none",
                                         connectionstyle="angle3,angleA=0,angleB=-90"))
 
-        plt.plot(event.summed_waveform(), label='summed')
-        plt.plot(event.filtered_waveform('filtered_for_large_s2'), label='large_s2')
-        plt.plot(event.filtered_waveform('filtered_for_small_s2'), label='small_s2')
+        plt.plot(event.filtered_waveform('uncorrected_sum_waveform_for_s1'), label='S1 peakfinding')
+        plt.plot(event.filtered_waveform('uncorrected_sum_waveform_for_s2'), label='S2 peakfinding')
+        plt.plot(event.filtered_waveform('filtered_for_large_s2'), label='Large s2 filtered')
+        plt.plot(event.filtered_waveform('filtered_for_small_s2'), label='Small s2 filtered')
+        event_length = len(event.summed_waveform())-1 #Want this as field in event structure...
+        plt.plot([0.6241506363] * event_length,  '--', label='Large S2 threshold')
+        plt.plot([0.06241506363]* event_length,  '--', label='Small S2 threshold')
+        plt.plot([0.1872451909] * event_length,  '--', label='S1 threshold')
 
-        plt.legend()
-        plt.xlabel('Time in event [10 ns]')
+        legend = plt.legend(loc='upper left', prop={'size':10})
+        legend.get_frame().set_alpha(0.5)
+        plt.xlabel('Sample in event [10 ns]')
         plt.ylabel("pe / bin")
+        plt.tight_layout()
 
         plt.show(block=False)
         self.log.info("Hit enter to continue...")
