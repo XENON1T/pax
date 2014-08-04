Speedup convolution bug implementation, uncorrected waveform computation
Should now be as good as, or better than, 0.2.0

plugins/dsp/AssembleSignals.py
@@ -106,30 +106,28 @@ class JoinAndConvertWaveforms(plugin.TransformPlugin):
                     # How much baseline will we have in 1T? Only few samples?
                     """
 
-                # Temp for Xerawdp matching: add pulse to the uncorrected sum waveform if they are not excluded
-                if not (channel > 178 or channel in self.config['pmts_excluded_for_s1']):
-                    #print(starting_position, len(wave_occurrence))
-                    uncorrected_sum_wave_for_s1[starting_position:starting_position + len(wave_occurrence)] = \
-                        np.add(-1 * (wave_occurrence - baseline) * self.conversion_factor / (2*10**6),
-                            uncorrected_sum_wave_for_s1[starting_position:starting_position + len(wave_occurrence)]
-                        )
-                if not channel > 178:
-                    uncorrected_sum_wave_for_s2[starting_position:starting_position + len(wave_occurrence)] = \
-                        np.add(-1 * (wave_occurrence - baseline) * self.conversion_factor / (2*10**6),
-                            uncorrected_sum_wave_for_s2[starting_position:starting_position + len(wave_occurrence)]
-                        )
+                corrected_pulse = baseline - wave_occurrence   # Note: flips up!
 
                 # Put wave occurrences in the correct positions
+
+                # Temp for Xerawdp matching: add pulse to the uncorrected sum waveform if they are not excluded
+                if not channel > 178:
+                    uncorrected_sum_wave_for_s2[starting_position:starting_position + len(wave_occurrence)] += corrected_pulse
+                    if not channel in self.config['pmts_excluded_for_s1']:
+                        uncorrected_sum_wave_for_s1[starting_position:starting_position + len(wave_occurrence)] += corrected_pulse
+
                 if skip_channel: continue
-                wave[starting_position:starting_position + len(wave_occurrence)] = wave_occurrence - baseline
+                wave[starting_position:starting_position + len(wave_occurrence)] = corrected_pulse
 
             if skip_channel: continue
-            # Flip the waveform up, convert it to pe/ns, and store it in the event data structure
-            event['channel_waveforms'][channel] = -1 * wave * self.conversion_factor / self.gains[channel]
+            # Convert wave to pe/ns, and store it in the event data structure
+            event['channel_waveforms'][channel] = wave * self.conversion_factor / self.gains[channel]
 
         # Temp for Xerawdp matching: store uncorrected sum waveform
-        event['processed_waveforms']['uncorrected_sum_waveform_for_s1'] = uncorrected_sum_wave_for_s1
-        event['processed_waveforms']['uncorrected_sum_waveform_for_s2'] = uncorrected_sum_wave_for_s2
+        universal_gain_correcion = self.conversion_factor / (2*10**6)
+        event['processed_waveforms']['uncorrected_sum_waveform_for_s1'] = uncorrected_sum_wave_for_s1 * universal_gain_correcion
+        event['processed_waveforms']['uncorrected_sum_waveform_for_s2'] = uncorrected_sum_wave_for_s2 * universal_gain_correcion
+
         # Delete the channel_occurrences from the event structure, we don't need it anymore
         del event['channel_occurrences']
 

plugins/dsp/Filtering.py
@@ -40,15 +40,20 @@ class GenericFilter(plugin.TransformPlugin):
         if not 'pulse_boundaries' in event:
              event['pulse_boundaries'] = {}
         if not self.input_name in event['pulse_boundaries']:
-            #Find the pulse boundaries in this input waveform - stupid slow code
-            previous = 0
-            pbs = []
-            for i,x in enumerate(signal):
-                if x==0 and previous != 0:
-                    pbs.append(('r',i-1))
-                if x!=0 and previous == 0:
-                    pbs.append(('l',i))
-                previous = x
+            # Find the pulse boundaries in this input waveform - stupid slow code
+            # previous = 0
+            # pbs = []
+            # for i,x in enumerate(signal):
+            #     if x==0 and previous != 0:
+            #         pbs.append(i-1)
+            #     if x!=0 and previous == 0:
+            #         pbs.append(i)
+            #     previous = x
+            # print(pbs)
+            # Gotta love numpy ;-)
+            # [0] and double parens are stupid though, but quite necessary here:
+            y = np.abs(np.sign(signal))
+            pbs = np.concatenate((np.where(np.roll(y,1) - y == -1)[0], np.where(np.roll(y,-1) - y == -1)[0]))
             event['pulse_boundaries'][self.input_name] = pbs
         # Mutilate the waveform
         # First mutilate the edges, which are always pulse boundaries
@@ -56,10 +61,9 @@ class GenericFilter(plugin.TransformPlugin):
         output[len(output)-int(filter_length/2):] = np.zeros(int(filter_length/2))
         # Mutilate waveform around pulse boundaries
         for pb in event['pulse_boundaries'][self.input_name]:
-            if pb[1] < filter_length/2: continue  #Too soon
             try:
-                lefti  = max(0,pb[1]-int(filter_length/2))
-                righti = min(len(signal)-1, pb[1]+int(filter_length/2))
+                lefti  = max(0,pb-int(filter_length/2))
+                righti = min(len(signal)-1, pb+int(filter_length/2))
                 output[lefti:righti] = np.zeros(righti-lefti)
             except Exception as e:
                 self.log.warning("Error during waveform mutilation: " + str(e) + ". So what...")