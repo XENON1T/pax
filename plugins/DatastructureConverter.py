233851cdaeb37a79a85e49ef6ec63756591d9159
Christopher Tunnell <ctunnell@nikhef.nl>
2014-08-11 23:09:20 +0200

    Remove clustering and DatastructureConverter that are broken and irreleveant, respectively

--- plugins/DatastructureConverter.py
+++
@@ -1,9 +0,0 @@
-from pax import datastructure
-from pax import plugin
-
-
-class ConvertToEventClass(plugin.TransformPlugin):
-
-    def transform_event(self, event):
-        event = datastructure.Event(event)
-        return event
