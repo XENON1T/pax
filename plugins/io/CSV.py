d3de55ac8f83f29baac222be930a5a52307048f8
Christopher Tunnell <ctunnell@nikhef.nl>
2014-08-29 11:17:32 +0200

    Cleanup and fix CSVWriter.  Also, docstring.  Closes #44.

--- plugins/io/CSV.py
+++ plugins/io/CSV.py
@@ -4,26 +4,28 @@ from pax import plugin
 
 
 class WritePeaksCSV(plugin.OutputPlugin):
+    """Write to a human readable CSV file
+    """
+
+    def startup(self):
+        self.output = open('output.csv', 'w')
+        self.headers = ['event', 'type', 'max', 'left', 'right', 'area']
+        self.csv = csv.DictWriter(self.output,
+                                  self.headers, lineterminator='\n')
+        self.csv.writeheader()
+
 
     def write_event(self, event):
         event.peaks.sort(key=lambda p: p.index_of_maximum)
         for p in event.peaks:
-            data = (
-                ('event', event.event_number),
-                ('type',  p.type),
-                ('max',   p.index_of_maximum),
-                ('left',  p.left),
-                ('right', p.right),
-                ('area',  p.area),
-            )
-            if not hasattr(self, 'csv'):
-                # If I do this in startup, I have to copy-paste the fieldnames
-                self.output = open('output.csv', 'w')
-                self.headers = [q[0] for q in data]
-                self.csv = csv.DictWriter(
-                    self.output, self.headers, lineterminator='\n')
-                self.csv.writeheader()
-            self.csv.writerow({a[0]: a[1] for a in data})
+            data = (event.event_number,
+                    p.type,
+                    p.index_of_maximum,
+                    p.left,
+                    p.right,
+                    p.area)
+
+            self.csv.writerow(dict(zip(self.keys, data)))
 
     def shutdown(self):
         self.output.close()
