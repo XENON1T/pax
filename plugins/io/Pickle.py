76ee53d740d898fb4f5f1e0fdad770c191deccdb
Jelle Aalbers <jelleaalbers@gmail.com>
2014-09-10 15:41:49 +0200

    Pickle update, now uses gzip

--- plugins/io/Pickle.py
+++ plugins/io/Pickle.py
@@ -1,29 +1,33 @@
-"""Pickle events
-
-Write event class to a file.
+"""Read/write event class from/to gzip-compressed pickle files.
 """
 
 from pax import plugin
 
+import gzip, re, glob
 try:
     import cPickle as pickle
 except:
     import pickle
 
 
+
 class WriteToPickleFile(plugin.OutputPlugin):
 
-    def startup(self):
-        self.log.debug("Writing pickled data to %s" %
-                       self.config['picklefile'])
-        self.file = open(self.config['picklefile'],
-                         'wb')
-
     def write_event(self, event):
-        self.log.debug('Pickling event')
-        pickle.dump(event,
-                    self.file)
+        with gzip.open(self.config['output_dir'] + '/' + str(event.event_number), 'wb') as file:
+            pickle.dump(event, file)
 
-    def shutdown(self):
-        self.log.debug("Closing %s" % self.config['picklefile'])
-        self.file.close()
+
+
+class DirWithPickleFiles(plugin.InputPlugin):
+
+    def get_events(self):
+        files = glob.glob(self.config['input_dir'] + "/*")
+        if len(files)==0:
+            self.log.fatal("No files found in input directory %s!" % self.config['input_dir'])
+        for file in files:
+            if re.search('(\d+)$',file) is None:
+                self.log.debug("Invalid file %s" % file)
+                continue
+            with gzip.open(file,'rb') as f:
+                yield pickle.load(f)
