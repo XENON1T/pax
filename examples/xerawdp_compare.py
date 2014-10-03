8e58dbd6f59f251ed3d6db448187414bbce5b8af
Christopher Tunnell <ctunnell@nikhef.nl>
2014-10-03 16:56:14 +0200

    XeRawDP comparison script update

--- examples/xerawdp_compare.py
+++ examples/xerawdp_compare.py
@@ -40,21 +40,20 @@ for i in range(root_trees['T1'].GetEntries()):
     time = np.uint64(root_trees['T1'].TimeSec) * np.uint64(1e8)
     time += np.uint64(root_trees['T1'].TimeMicroSec) * np.uint64(100)
     time += np.uint64(root_trees['T2'].S2sPeak[0])
-    time *= 10
+    time *= np.uint64(10)
 
     # Find event number
     this_event = None
     for event in pax_file.root.event_table:
         if event['start_time'] < time < event['stop_time']:
-            if this_event is None:
-                this_event = event
-            else:
-                raise RuntimeError("Overlapping events?")
+            this_event = event
+            break
 
-    for peak in pax_file.root.peak_table.where("(event_number == %d) & (type == b's2')" % event['event_number']):
-        start = peak['left'] * event['sample_duration'] + event['start_time']
-        maximum = peak['index_of_maximum'] * event['sample_duration'] + event['start_time']
-        stop = peak['right'] * event['sample_duration'] + event['start_time']
+    for peak in pax_file.root.peak_table.where("(event_number == %d) & (type == b's2')" % this_event['event_number']):
+        assert this_event['start_time'] < time < this_event['stop_time']
+
+        start = peak['left'] * this_event['sample_duration'] + this_event['start_time']
+        stop = peak['right'] * this_event['sample_duration'] + this_event['start_time']
 
         if start < time < stop:
             print(root_trees['T2'].S2sTot[0],
@@ -62,14 +61,25 @@ for i in range(root_trees['T1'].GetEntries()):
             variables_to_compare['S2[0]'].append((root_trees['T2'].S2sTot[0],
                                                   peak['area']))
 
-print(variables_to_compare['S2[0]'])
 
-x, y = zip(*variables_to_compare['S2[0]'])
-print(x,y)
+for k, v in variables_to_compare.items():
+    print(k)
 
-plt.plot(x, y)
-plt.xlabel('root')
-plt.ylabel('pax')
+    x, y = zip(*v)
+    x, y = np.array(x), np.array(y)
+    print(x,y)
+
+    plt.figure()
+    plt.title(k)
+    plt.scatter(x, y)
+    plt.xlabel('root')
+    plt.ylabel('pax')
+
+    plt.figure()
+    plt.title(k)
+    plt.hist((x-y)/x)
+    plt.xlabel('root - pax')
+
 plt.show()
 
 
