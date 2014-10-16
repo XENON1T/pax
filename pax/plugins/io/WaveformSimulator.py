b43df2a449538a880f8adef262fc7a603624f846
Jelle Aalbers <jelleaalbers@gmail.com>
2014-10-16 14:12:24 +0200

    Fax bugfixes, truth file CSV format

--- pax/plugins/io/WaveformSimulator.py
+++ pax/plugins/io/WaveformSimulator.py
@@ -5,35 +5,85 @@ try:
     import cPickle as pickle
 except:
     import pickle
-from pax import plugin, units, datastructure, simulation
 
+from pax import plugin, units, datastructure, simulation
 
 class WaveformSimulator(plugin.InputPlugin):
     """ Common I/O for waveform simulator: truth file writing, pax event creation, etc.
     There is no physics here: all that is in pax.simulation
     """
     def startup(self):
-        # Open the truth file
-        self.truth_file = open(self.config['truth_filename'], 'wb')
+        self.truth_file = None
         self.config = simulation.init_config(self.config)
+
+    def write_truth(self, event_number, peaks):
+        """ Write stuff to the truth file
+        """
+        tff = self.config['truth_file_format']
+
+        if tff == 'stacked_pickles':
+            if self.truth_file is None:
+                self.truth_file = open(self.config['truth_filename'], 'wb')
+            pickle.dump({'event_number' : event_number, 'peaks' : peaks}, self.truth_file)
+
+        elif tff == 'csv_peaklist':
+            if self.truth_file is None:
+
+                headers = [
+                    'event_number', 'type',
+                    'x', 'y', 'z',
+                    't_interaction',
+                    'n_photons', 't_mean_photons', 't_first_photon', 't_last_photon', 't_sigma_photons',
+                    'n_electrons', 't_sigma_electrons', 't_mean_electrons', 't_first_electron', 't_last_electron'
+                ]
+                self.truth_file = csv.DictWriter(
+                    f = open(self.config['truth_file_name'], 'w'),
+                    fieldnames = headers,
+                    lineterminator='\n'
+                )
+                self.truth_file.writeheader()
+            for p in peaks:
+                p['event_number'] = event_number
+                self.truth_file.writerow(p)
+        else:
+            raise ValueError('Unsupported waveform simulator truth file format %s' % tff)
+
+    def store_true_peak(self, type, t, x, y, z, photon_times, electron_times=[]):
+        """ Saves the truth information about a peak (s1 or s2)
+        """
+        true_peak = {
+            'type': 's2',
+            'x': x, 'y': y, 'z': z,
+            't_interaction':     t,
+        }
+        for name, times in (('photon', photon_times), ('electron', electron_times)):
+            if len(times) != 0:
+                true_peak.update({
+                    ('n_%ss' % name):         len(times),
+                    ('t_mean_%ss' % name):    np.mean(times),
+                    ('t_first_%s' % name):    np.min(times),
+                    ('t_last_%s' % name):     np.max(times),
+                    ('t_sigma_%ss' % name):   np.std(times),
+                })
+            else:
+                true_peak.update({
+                    ('n_%ss' % name):         '',
+                    ('t_mean_%ss' % name):     '',
+                    ('t_first_%s' % name):    '',
+                    ('t_last_%s' % name):     '',
+                    ('t_sigma_%ss' % name):    '',
+                })
+        self.truth_peaks.append(true_peak)
 
     def s2(self, electrons, t=0., z=0., x=0., y=0.):
         electron_times = simulation.s2_electrons(electrons_generated=electrons, t=t, z=z)
         photon_times = simulation.s2_scintillation(electron_times)
-        self.truth_peaks.append({
-            'signal_type': 's2',
-            'x': x, 'y': y, 'z': z,
-            't_interaction': t,
-            'photons': len(photon_times),
-            't_photons': np.mean(photon_times),
-            'electrons': len(electron_times),
-            'e_cloud_sigma': np.std(electron_times),
-        })
+        self.store_true_peak('s2', t, x, y, z, photon_times, electron_times)
         return simulation.hitlist_to_waveforms(
             simulation.photons_to_hitlist(photon_times),
         )
 
-    def s1(self, photons, t=0, recombination_time=None, singlet_fraction=None):
+    def s1(self, photons, t=0, recombination_time=None, singlet_fraction=None, x=0., y=0., z=0.):
         """
         :param photons: total # of photons generated in the S1
         :param t: Time at which the interaction occurs, i.e. offset for arrival times. Defaults to s1_default_recombination_time
@@ -42,23 +92,23 @@ class WaveformSimulator(plugin.InputPlugin):
         :return: start_time, pmt_waveforms
         """
         photon_times = simulation.s1_photons(photons, t, recombination_time, singlet_fraction)
-        self.truth_peaks.append({
-            'peak_type': 's1',
-            #'x': x, 'y': y, 'z': z,
-            't_interaction': t,
-            'photons': len(photon_times),
-            't_photons': np.mean(photon_times),
-        })
+        self.store_true_peak('s1', t, x, y, z, photon_times)
         return simulation.hitlist_to_waveforms(
             simulation.photons_to_hitlist(photon_times),
         )
 
+    def get_instructions_for_next_event(self):
+        raise NotImplementedError()
+
     def get_events(self):
 
         dt = self.config['digitizer_t_resolution']
-        event_number = 0
-        for instructions in self.get_instructions_for_next_event():
+        for instruction_number, instructions in enumerate(self.get_instructions_for_next_event()):
             for repetition_i in range(self.config['event_repetitions']):
+                event_number = instruction_number * self.config['event_repetitions'] + repetition_i
+                self.log.debug('Instruction %s, iteration %s, event number %s' % (
+                    instruction_number, repetition_i, event_number
+                ))
                 self.truth_peaks = []
 
                 # Make (start_time, waveform matrix) tuples for every s1/s2 we have to generate
@@ -68,16 +118,17 @@ class WaveformSimulator(plugin.InputPlugin):
                         q['s1_photons'], q['s2_electrons'], q['depth'], q['t']
                     ))
                     if int(q['s1_photons']):
-                        signals += [self.s1(
-                            int(q['s1_photons']),
-                            t=float(q['t'])
-                        )]
+                        signals.append(
+                            self.s1( photons=int(q['s1_photons']), t=float(q['t']))
+                        )
                     if int(q['s2_electrons']):
-                        signals += [self.s2(
-                            int(q['s2_electrons']),
-                            z=float(q['depth']) * units.cm,
-                            t=float(q['t'])
-                        )]
+                        signals.append(
+                            self.s2(
+                                electrons=int(q['s2_electrons']),
+                                z=float(q['depth']) * units.cm,
+                                t=float(q['t'])
+                             )
+                        )
 
                 # Remove empty signals (None) from signal list
                 signals = [s for s in signals if s is not None]
@@ -86,16 +137,17 @@ class WaveformSimulator(plugin.InputPlugin):
                         "Fax simulation returned no signals, can't return an event...")
                     continue
 
-                # Combine everything into a single waveform matrix
-                # Baseline addition & flipping down is done here
-                self.log.debug("Combining %s signals into a single matrix" % len(signals))
+                # Compute start time and event length in samples)
                 start_time_offset = min([s[0] for s in signals])
-                # Compute event length in samples
                 event_length = int(
                      2 * self.config['event_padding'] / dt +
                      max([s[0] - start_time_offset for s in signals]) / dt +
                      max([s[1].shape[1] for s in signals])
                 )
+
+                # Make a single waveform matrix
+                # Baseline addition & flipping down is done here
+                self.log.debug("Combining %s signals into a single matrix" % len(signals))
                 pmt_waveforms = self.config['digitizer_baseline'] *\
                                 np.ones((len(self.config['channels']), event_length), dtype=np.int16)
                 for s in signals:
@@ -104,19 +156,19 @@ class WaveformSimulator(plugin.InputPlugin):
                     )
                     # NOTE: MINUS!
                     pmt_waveforms[:, start_index:start_index + s[1].shape[1]] -= s[1]
+
                 # Clipping
                 pmt_waveforms = np.clip(pmt_waveforms, 0, 2 ** (self.config['digitizer_bits']))
 
-                # Create and yield a pax event
+                # Setup the pax event 'header'
                 self.log.debug("Creating pax event")
                 event = datastructure.Event()
                 event.event_number = event_number
-                now = int(time.time() * units.s)
-                event.start_time = int(now)
+                event.start_time = int(time.time() * units.s)
                 event.stop_time = event.start_time + int(event_length * dt)
                 event.sample_duration = dt
-                # Make a single occurrence for the entire event... yeah, this
-                # should be changed
+
+                # Make a single occurrence for the entire event... yeah, this is wonky
                 event.occurrences = {
                     ch: [(0, pmt_waveforms[ch])]
                     for ch in self.config['channels']
@@ -128,17 +180,15 @@ class WaveformSimulator(plugin.InputPlugin):
                 # Write the truth of the event
                 # Remove start time offset from all times in the peak
                 for p in self.truth_peaks:
-                    p['t_interaction'] -= start_time_offset
-                    p['t_photons'] -= start_time_offset
-                self.write_truth({
-                    'event_number' : event_number,
-                    'peaks' : self.truth_peaks
-                })
+                    for key in p.keys():
+                        if key[:2] == 't_' and key[2:7] != 'sigma':
+                            if p[key] == '':
+                                continue
+                            p[key] -= start_time_offset
+                self.write_truth(event_number=event_number, peaks=self.truth_peaks)
 
                 event_number += 1
 
-    def write_truth(self, stuff):
-        pickle.dump(stuff, self.truth_file)
 
 
 
@@ -153,16 +203,16 @@ class WaveformSimulatorFromCSV(WaveformSimulator):
         self.instructions_file.close()
 
     def get_instructions_for_next_event(self):
-        this_event = 0
-        this_event_peaks = []
+        this_instruction = None
+        this_instruction_peaks = []
         for p in self.instructions:
-            if int(p['event']) == this_event:
-                this_event_peaks.append(p)
+            if int(p['instruction']) == this_instruction:
+                this_instruction_peaks.append(p)
             else:
                 # New event reached!
-                # How often do we need to repeat the old event?
-                yield this_event_peaks
-                this_event = int(p['event'])
-                this_event_peaks = [p]
+                if this_instruction_peaks:
+                    yield this_instruction_peaks
+                this_instruction = int(p['instruction'])
+                this_instruction_peaks = [p]
         # For the final event...
-        yield this_event_peaks
+        yield this_instruction_peaks
