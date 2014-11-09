b1c4c61fd7e0aaf6486a3e41ef299b8467abbb70
Jelle Aalbers <jelleaalbers@gmail.com>
2014-11-09 14:42:01 +0100

    Plotting: summary/combination plot, enhancements

--- pax/plugins/io/Plotting.py
+++ pax/plugins/io/Plotting.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 import os
+import time
 from mpl_toolkits.mplot3d import Axes3D
 
 from pax import plugin, units
@@ -21,6 +22,8 @@ class PlotBase(plugin.OutputPlugin):
             self.output_dir = None
 
         self.skip_counter = 0
+        # Convert times in samples to times in us: need to know how many samples / us
+        self.samples_to_us = self.config['digitizer_t_resolution'] / units.us
 
         self.substartup()
 
@@ -36,16 +39,13 @@ class PlotBase(plugin.OutputPlugin):
             return
 
         self.plot_event(event)
-
         self.finalize_plot(event.event_number)
 
     def plot_event(self, event):
         raise NotImplementedError()
 
     def finalize_plot(self, num = 0):
-        """Finalize plotting and send to screen/file
-
-        Call this instead of plt.show()
+        """Finalize plotting, send to screen/file, then closes plot properly (avoids runtimewarning / memory leak).
         """
         if self.output_dir:
             plt.savefig(self.output_dir + '/' + str(num) + '.png')
@@ -53,101 +53,123 @@ class PlotBase(plugin.OutputPlugin):
             plt.show(block=False)
             self.log.info("Hit enter to continue...")
             input()
-            plt.close()
+        plt.close()
         self.skip_counter = self.config['plot_every'] - 1
 
 
-class PlotWaveform(PlotBase):
-    def plot_event(self, event):
-        """Plot an event
-
-        Will make a fancy plot with lots of arrows etc of a summed waveform
+    def plot_waveform(self, event, left=0, right=None, pad=0, show_peaks=False, show_legend=True, log_y_axis=False):
         """
-
-        self.time_conversion_factor = event.sample_duration * units.ns / units.us
-
-        rows = 2
-        cols = 3
-
-        plt.figure(figsize=(6 * cols, 4 * rows))
-
-        # Plot the largest S1 and largest S2 in the event
-        if event.S1s():
-            ax = plt.subplot2grid((rows, cols), (0, 0))
-            largest_s1 = sorted(
-                event.S1s(), key=lambda x: x.area, reverse=True)[0]
-            self.plot_waveform(event, left=largest_s1.left,
-                               right=largest_s1.right, pad=10)
-
-            time = largest_s1.index_of_maximum * self.time_conversion_factor
-
-            plt.title("S1 at %.1f us" % time)
-
-        if event.S2s():
-            ax = plt.subplot2grid((rows, cols), (0, 1))
-            ax.yaxis.set_label_position("right")
-
-            largest_s2 = sorted(event.S2s(), key=lambda x: x.area,
-                                reverse=True)[0]
-
-            pad = 200 if largest_s2.height > 100 else 50
-
-            self.plot_waveform(event, left=largest_s2.left,
-                               right=largest_s2.right, pad=pad)
-
-            time = largest_s2.index_of_maximum * self.time_conversion_factor
-
-            plt.title("S2 at %.1f us" % time)
-
-
-        # Plot the total waveform
-        plt.subplot2grid((rows, cols), (rows - 1, 0), colspan=cols)
-        self.plot_waveform(event, show_peaks=True)
-        legend = plt.legend(loc='upper left', prop={'size': 10})
-        if legend and legend.get_frame():
-            legend.get_frame().set_alpha(0.5)
-        plt.tight_layout()
-
-    def plot_waveform(self, event, left=0, right=None, pad=0, show_peaks=False):
+        Plot part of an event's sum waveform. Defined in base class to ensure a uniform style
+        """
         if right is None:
             right = event.length() - 1
         lefti = max(0, left - pad)
         righti = min(right + pad, event.length() - 1)
         nsamples = 1 + righti - lefti
         dt = event.sample_duration * units.ns
-        xlabels = np.arange(lefti * dt / units.us, # 10+ labels will be cut later, prevents off by one errors     
+
+        xlabels = np.arange(lefti * dt / units.us, # 10+ labels will be cut later, prevents off by one errors
                             (10 + righti) * dt / units.us,
                             dt / units.us)
         xlabels = xlabels[:nsamples]
+
+
         plt.autoscale(True, axis='both', tight=True)
+
+        if log_y_axis:
+            plt.yscale('log')
+            plt.ylabel('Amplitude + 1 (pe/bin)')
+            y_offset = 1
+        else:
+            plt.ylabel('Amplitude (pe/bin)')
+            y_offset = 0
+        plt.xlabel('Time (us)')
+
         for w in self.config['waveforms_to_plot']:
             waveform = event.get_waveform(w['internal_name'])
             plt.plot(xlabels,
-                     waveform.samples[lefti:righti + 1],
+                     waveform.samples[lefti:righti + 1] + y_offset,
                      label=w['plot_label'])
+        if log_y_axis:
+            plt.ylim((0.9,plt.ylim()[1]))
 
-        plt.ylabel('Amplitude (pe/bin)')
-        plt.xlabel('Time (us)')
 
         if show_peaks and event.peaks:
-            # Plot all peaks
             max_y = max([p.height for p in event.peaks])
 
             for peak in event.peaks:
-                x = peak.index_of_maximum * self.time_conversion_factor
+                x = peak.index_of_maximum * self.samples_to_us
                 y = peak.height
-                plt.hlines(y, peak.left * self.time_conversion_factor,
-                           peak.right * self.time_conversion_factor)
+                plt.hlines(y, peak.left * self.samples_to_us,
+                           peak.right * self.samples_to_us)
+                if log_y_axis:
+                    ytext = y + (1-y/max_y)
+                    arrowprops = None
+                else:
+                    ytext =  y + (max_y - y) * (
+                                0.05 + 0.2 * random.random()
+                             )
+                    arrowprops = dict(arrowstyle="fancy",
+                                      fc="0.6", ec="none",
+                                      connectionstyle="angle3,"
+                                                      "angleA=0,"
+                                                      "angleB=-90")
                 plt.annotate('%s:%s' % (peak.type, int(peak.area)),
                              xy=(x, y),
-                             xytext=(
-                                 x, y + (max_y - y) * (
-                                 0.05 + 0.2 * random.random())),
-                             arrowprops=dict(arrowstyle="fancy",
-                                             fc="0.6", ec="none",
-                                             connectionstyle="angle3,"
-                                                             "angleA=0,"
-                                                             "angleB=-90"))
+                             xytext=(x, ytext),
+                             arrowprops = arrowprops)
+        if show_legend:
+            legend = plt.legend(loc='upper left', prop={'size': 10})
+            if legend and legend.get_frame():
+                legend.get_frame().set_alpha(0.5)
+
+
+
+class PlotSumWaveformLargestS2(PlotBase):
+
+    def plot_event(self, event, show_legend=False):
+        if not event.S2s():
+            self.log.warning("Can't plot the largest S2: there aren't any S2s in this event.")
+            plt.title('No S2 in event')
+            return
+
+        largest_s2 = sorted(event.S2s(), key=lambda x: x.area,
+                            reverse=True)[0]
+
+        pad = 200 if largest_s2.height > 100 else 50
+
+        self.plot_waveform(event, left=largest_s2.left, right=largest_s2.right,
+                           pad=pad, show_legend=show_legend, log_y_axis=self.config['log_scale_s2'])
+
+        time = largest_s2.index_of_maximum * self.samples_to_us
+
+        plt.title("S2 at %.1f us" % time)
+
+
+class PlotSumWaveformLargestS1(PlotBase):
+
+    def plot_event(self, event, show_legend=False):
+        if not event.S1s():
+            self.log.warning("Can't plot the largest S1: there aren't any S1s in this event.")
+            plt.title('No S1 in event')
+            return
+
+        largest_s1 = sorted(event.S1s(), key=lambda x: x.area,
+                            reverse=True)[0]
+
+        self.plot_waveform(event, left=largest_s1.left, right=largest_s1.right,
+                           pad=10, show_legend=show_legend, log_y_axis=self.config['log_scale_s1'])
+
+        time = largest_s1.index_of_maximum * self.samples_to_us
+
+        plt.title("S1 at %.1f us" % time)
+
+
+class PlotSumWaveformEntireEvent(PlotBase):
+
+    def plot_event(self, event, show_legend=True):
+        self.plot_waveform(event, show_peaks=True, show_legend=show_legend,
+                           log_y_axis=self.config['log_scale_entire_event'])
 
 
 class PlottingHitPattern(PlotBase):
@@ -156,36 +178,40 @@ class PlottingHitPattern(PlotBase):
         self.pmts_bottom = self.config['pmts_bottom']
         self.pmt_locations = self.config['pmt_locations']
 
-    def _plot(self, peak, ax, pmts):
+    def _plot(self, peak, ax, pmts, size_multiplication_factor=1):
         area = []
         points = []
         for pmt in pmts:
-            area.append(peak.area_per_pmt[pmt] / 10)
-
+            area.append(peak.area_per_pmt[pmt])
             points.append((self.pmt_locations[pmt]['x'],
                            self.pmt_locations[pmt]['y']))
 
-
-        c = ax.scatter(*zip(*points), c='red',
-                        s=area, cmap=plt.cm.hsv)
+        area = np.array(area)*size_multiplication_factor
+        total_area = np.sum(area)
+        c = ax.scatter(*zip(*points), s=5000*area/total_area, c=area, cmap=plt.cm.hot)
         c.set_alpha(0.75)
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
 
-    def plot_event(self, event):
-        f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
+    def plot_event(self, event, show=['S1','S2'], show_dominant_array_only=True, subplots_to_use=None):
+        if subplots_to_use is None:
+            f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
+            subplots_to_use = [ax1, ax2, ax3, ax4]
 
-        if event.S2s() and len(event.S2s()) >= 1:
-            S2 = event.S2s()[0]
-            ax1.set_title('S2 top (from above?)')
-            self._plot(S2, ax1, self.pmts_top)
-            ax2.set_title('S2 bottom (from above?)')
-            self._plot(S2, ax2, self.pmts_bottom)
+        for peak_type, dominant_array in (
+                ('S1', 'bottom'), ('S2', 'top')):
+            if peak_type not in show:
+                continue
+            peak_list = getattr(event, peak_type+'s')()
+            if peak_list and len(peak_list) >= 1:
+                peak = peak_list[0]
+                for array in ('top', 'bottom'):
+                    if show_dominant_array_only and array != dominant_array:
+                        continue
+                    ax = subplots_to_use.pop(0)
+                    ax.set_title('%s %s' % (peak_type, array))
 
-        if event.S1s() and len(event.S1s()) >= 1:
-            S1 = event.S1s()[0]
-            ax3.set_title('S1 top (from above?)')
-            self._plot(S1, ax3, self.pmts_top)
-            ax4.set_title('S1 bottom (from above?)')
-            self._plot(S1, ax4, self.pmts_bottom)
+                    self._plot(peak, ax, getattr(self, 'pmts_%s'%array))
 
             
             
@@ -228,3 +254,43 @@ class PlotChannelWaveforms(PlotBase): # user sets variables xlim, ylim for 3D pl
         ax.set_ylim3d(ylim_channel_start, ylim_channel_end)
         
         ax.set_zlabel('Pulse height')
+
+
+class PlotEventSummary(PlotBase):
+    def plot_event(self, event, plt=plt):
+        """
+        Combines several plots into a nice summary plot
+        """
+
+        rows = 2
+        cols = 4
+
+        plt.figure(figsize=(4 * cols, 4 * rows))
+        title = 'Event %s from %s -- recorded at %s UTC, %09dns' % (
+                        event.event_number, event.dataset_name,
+                        time.strftime("%Y/%m/%d, %H:%M:%S", time.gmtime(event.start_time/10**9)),
+                         event.start_time%(10**9))
+        plt.suptitle(title, fontsize=18)
+
+        plt.subplot2grid((rows, cols), (0, 0))
+        q = PlotSumWaveformLargestS1(self.config)
+        q.plot_event(event, show_legend=False)
+
+        ax = plt.subplot2grid((rows, cols), (0, 3))
+        ax.yaxis.set_label_position("right")
+        ax.yaxis.set_ticks_position("right")
+        q = PlotSumWaveformLargestS2(self.config)
+        q.plot_event(event, show_legend=False)
+
+        q = PlottingHitPattern(self.config)
+        q.plot_event(event, show_dominant_array_only=True, subplots_to_use = [
+                plt.subplot2grid((rows, cols), (0, 1)),
+                plt.subplot2grid((rows, cols), (0, 2))
+            ])
+
+        plt.subplot2grid((rows, cols), (rows - 1, 0), colspan=cols)
+        q = PlotSumWaveformEntireEvent(self.config)
+        q.plot_event(event, show_legend=True)
+
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.88)
