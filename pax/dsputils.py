newDSP: bugfix in left peak bound computation

pax/dsputils.py
@@ -73,7 +73,7 @@ def find_peak_in_signal(signal, unfiltered, integration_bound_fraction, offset=0
     left, right = peak_bounds(signal, max_idx, integration_bound_fraction)
     # Compute properties of this peak using 'unfiltered'
     area = np.sum(unfiltered[left:right + 1])
-    unfiltered_max = np.argmax(unfiltered[left:right + 1])
+    unfiltered_max = left + np.argmax(unfiltered[left:right + 1])
     return datastructure.Peak({
         'index_of_maximum': offset + unfiltered_max,
         'height':           unfiltered[unfiltered_max],
@@ -95,6 +95,10 @@ def peak_bounds(signal, max_idx, fraction_of_max, zero_level=0):
     """
     if len(signal) == 0:
         raise RuntimeError("Empty signal, can't find peak bounds!")
+    if max_idx > len(signal)-1:
+        raise RuntimeError("Can't compute bounds: max at %s, peak wave is %s long" % (max_idx, len(signal)))
+    if max_idx < 0:
+        raise RuntimeError("Peak maximum index is negative (%s)... what are you smoking?" % max_idx )
 
     height = signal[max_idx]
     threshold = max(zero_level, height * fraction_of_max)

pax/plugins/dsp/newDSP.py
@@ -94,8 +94,13 @@ class ComputePeakProperties(plugin.TransformPlugin):
                 np.where(peak.area_per_pmt >= self.config['minimum_pe_area'])[0],
                 dtype=np.uint16)
 
-            # Compute the widths
-            # This is quite expensive...
+            # Compute the widths. This is quite expensive...
+
+            if peak.index_of_maximum < peak.left:
+                self.log.debug("Insane peak %s-%s-%s, can't compute widths!" % (
+                    peak.left, peak.index_of_maximum, peak.right))
+                continue
+
             peak.full_width_half_max = dsputils.width_at_fraction(
                 peak_wave=unfiltered,
                 fraction_of_max=0.5,