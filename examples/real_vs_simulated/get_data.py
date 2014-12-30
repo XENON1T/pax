060f64820481ec7ce3fb0a3e5848e054529bec16
Jelle Aalbers <jelleaalbers@gmail.com>
2014-12-30 12:01:43 +0100

    Examples: improved real_vs_simulated Changed S2 specification to take account for drift time. Plot some example waveforms. More statistical parameters to compare. Changed a few settings to give better simulator-data agreement.

--- examples/real_vs_simulated/get_data.py
+++ examples/real_vs_simulated/get_data.py
@@ -1,3 +1,13 @@
+"""
+Generates some processor output for real data and simulated data.
+
+The electron lifetime is set to 'infinity', so the area of S2s
+is more under control.
+
+1/100 waveforms are plot so you can verify they are sensible.
+ -- this is always a good idea for simulated data!
+"""
+
 from pax import core, units
 
 print("Processing the real data...")
@@ -6,11 +16,15 @@ core.Processor(config_names=['XENON100', 'newDSP'], config_dict={
         'input_name'  :     'xe100_120402_2000_000000.xed',
         'output_name' :     'xe100_120402_2000_000000',
         'input' :           'XED.XedInput',
-        'output':           'Pandas.WritePandas',
+        'output':           ['Pandas.WritePandas','Plotting.PlotEventSummary']
     },
     'Pandas.WritePandas' : {
         'output_format': 'hdf',
-    }
+    },
+	'Plotting.PlotEventSummary' : {
+		'plot_every':	100,
+		'output_dir':	'real_data_waveforms'
+	}
 }).run()
 
 print("Simulating some fake data...")
@@ -20,13 +34,19 @@ for simulation_name in ('fake_s1s', 'fake_s2s'):
             'input_name'  :     simulation_name + '.csv',
             'output_name' :     simulation_name,
             'input' :           'WaveformSimulator.WaveformSimulatorFromCSV',
-            'output':           'Pandas.WritePandas',
+            'output':           ['Pandas.WritePandas','Plotting.PlotEventSummary'],
         },
         'WaveformSimulator' : {
             'event_repetitions' : 100,
             'electron_lifetime_liquid' : 1000 * units.s,
+			'wire_field_parameter': 0,
+			'drift_velocity_gas': 3*units.mm/units.us,
         },
         'Pandas.WritePandas' : {
             'output_format': 'hdf',
-        }
+        },
+		'Plotting.PlotEventSummary' : {
+			'plot_every':	100,
+			'output_dir':	simulation_name + '_waveforms',
+		}
     }).run()
