Fix off-by-one errors introduced by Occurrence class Also split up ClusterAndClassifySmallPeaks into two parts, and have a sensible height value for small peak clusters.

pax/plugins/io/Plotting.py
@@ -134,7 +134,7 @@ class PlotBase(plugin.OutputPlugin):
                                       connectionstyle="angle3,"
                                                       "angleA=0,"
                                                       "angleB=-90")
-                plt.hlines(y, peak.left * self.samples_to_us,
+                plt.hlines(y, (peak.left - 1) * self.samples_to_us,
                               peak.right * self.samples_to_us)
                 plt.annotate('%s:%s' % (peak.type, int(peak.area)),
                              xy=(x, y),
@@ -151,7 +151,7 @@ class PlotBase(plugin.OutputPlugin):
         # Separated so PlotChannelWaveforms2D can also call it
         for peak in event.peaks:
             shade_color = self.peak_colors.get(peak.type,'gray') if peak.detector == 'tpc' else 'red'
-            plt.axvspan(peak.left  * self.samples_to_us,
+            plt.axvspan((peak.left - 1)  * self.samples_to_us,
                         peak.right * self.samples_to_us,
                         color=shade_color,
                         alpha=0.2)
@@ -343,25 +343,18 @@ class PlotChannelWaveforms2D(PlotBase):
 
         # TODO: change from lines to squares
         for oc in event.occurrences:
-            start_index = oc.left
-            # Remember: intervaltree uses half-open intervals, end_index is the first index outside!
-            end_index = oc.right
-            length = end_index - start_index
-            channel = oc.channel
-            height = oc.height
-
             if oc.height is None:
                 # Maybe gain was 0 or something
                 # TODO: plot these too, in a different color
                 continue
 
             # Choose a color for this occurrence based on amplitude
-            color_factor = np.clip(np.log10(height)/2, 0, 1)
+            color_factor = np.clip(np.log10(oc.height)/2, 0, 1)
 
-            plt.gca().add_patch(Rectangle((start_index*time_scale, channel), length*time_scale, 1,
+            plt.gca().add_patch(Rectangle((oc.left*time_scale, oc.channel), oc.length*time_scale, 1,
                                           facecolor=plt.cm.gnuplot2(color_factor),
                                           edgecolor='none',
-                                          alpha=(0.1 if event.is_channel_bad[channel] else 1.0)))
+                                          alpha=(0.1 if event.is_channel_bad[oc.channel] else 0.7)))
 
         # Plot the channel peaks as dots
         # All these for loops are slow -- hope we get by-column access some time
@@ -373,8 +366,8 @@ class PlotChannelWaveforms2D(PlotBase):
                 color_factor = 1
             else:
                 color_factor = min(max(p.height/(20*p.noise_sigma), 0), 1)
-            plt.scatter(  [p.index_of_maximum * time_scale],
-                          [p.channel],
+            plt.scatter(  [(0.5 + p.index_of_maximum) * time_scale],
+                          [0.5 + p.channel],
                         c=(color_factor, 0, (1-color_factor)),
                         s=10*p.area,
                         edgecolor=(0.5*color_factor, 0, 0.5*(1-color_factor)),

pax/plugins/signal_processing/SmallPeakProcessing.py
@@ -2,18 +2,20 @@ import numpy as np
 from pax import plugin, datastructure, utils
 
 
-class ClusterAndClassifySmallPeaks(plugin.TransformPlugin):
+class ClusterSmallPeaks(plugin.TransformPlugin):
+    """Clusters individual channel peaks into groups, and labels them as noise / lone_pulse / unknown
+    'unknown' means an S1 or S2, which will be decided by a later plugin
+    """
 
     def startup(self):
         self.dt = dt = self.config['digitizer_t_resolution']
         self.cluster_separation_length = self.config['cluster_separation_time']
-        self.classification_mode = self.config['classification_mode']
 
     def transform_event(self, event):
 
         # Handle each detector separately
         for detector in self.config['channels_in_detector'].keys():
-            self.log.debug("Clustering and classifying channel peaks in data from %s" % detector)
+            self.log.debug("Clustering channel peaks in data from %s" % detector)
             peaks = []      # Superfluous, while loop is always run once... but pycharm complains if we omit
 
             # Hmzz, python has no do_while, so..
@@ -51,8 +53,9 @@ class ClusterAndClassifySmallPeaks(plugin.TransformPlugin):
                     peak.area =  sum([s.area  for s in peak.channel_peaks])
 
                     # For backwards compatibility with plotting code
-                    peak.index_of_maximum = int((peak.left + peak.right)/2)
-                    peak.height = 0
+                    highest_peak_index = np.argmax([s.height for s in peak.channel_peaks])
+                    peak.index_of_maximum = peak.channel_peaks[highest_peak_index].index_of_maximum
+                    peak.height = peak.channel_peaks[highest_peak_index].height
 
                     # Contributing channels are in the detector and have an spe
                     # (not in a bad channel, but those spes have already been filtered out)
@@ -81,7 +84,7 @@ class ClusterAndClassifySmallPeaks(plugin.TransformPlugin):
                     times = [s.index_of_maximum * self.dt for s in peak.channel_peaks]
                     peak.mean_absolute_deviation = utils.mad(times)
 
-                    # Simple ad-hoc classification
+                    # Classification for noise and lone_pulse peaks
 
                     if peak.number_of_noise_channels > 2 * peak.number_of_contributing_channels:
                         peak.type = 'noise'
@@ -92,13 +95,8 @@ class ClusterAndClassifySmallPeaks(plugin.TransformPlugin):
                         dark_count[channel] = dark_count.get(channel, 0) + 1
 
                     else:
-                        if peak.mean_absolute_deviation < 10:
-                            peak.type = 's1'
-                        else:
-                            if peak.number_of_contributing_channels < 5:
-                                peak.type = 'unknown'
-                            else:
-                                peak.type = 's2'
+                        # Peak classification done in a separate plugin
+                        peak.type = 'unknown'
 
                     peaks.append(peak)
 
@@ -110,9 +108,30 @@ class ClusterAndClassifySmallPeaks(plugin.TransformPlugin):
                         event.is_channel_bad[ch] = True
                         if self.config['exclude_bad_channels']:
                             redo_classification = True
-                            self.log.debug("Classification has to be redone!!")
+                            self.log.debug("Clustering has to be redone!!")
 
             # Classification is now done, so add the peaks to the datastructure
             event.peaks.extend(peaks)
+
+        return event
+
+
+class AdHocClassification(plugin.TransformPlugin):
+
+    def transform_event(self, event):
+
+        for peak in event.peaks:
+
+            # Work only on unknown peaks - not noise, lone_pulse, and big peaks (which are already s1 or s2)
+            if peak.type != 'unknown':
+                continue
+
+            if peak.mean_absolute_deviation < 10:
+                peak.type = 's1'
+                continue
+
+            if peak.number_of_contributing_channels >= 5:
+                peak.type = 's2'
+                continue
 
         return event

pax/plugins/signal_processing/SmallPeakfinder.py
@@ -54,7 +54,7 @@ class FindSmallPeaks(plugin.TransformPlugin):
                 if region_left >= give_up_after:
                     break
 
-                # Find all occurrences completely enveloped in the free region. Thank pyintervaltree for log(n) runtime
+                # Find all occurrences completely enveloped in the free region.
                 # TODO: we should put strict=False, so we're not relying on the zero-suppression to separate
                 # small peaks close to a large peak. However, right now this brings in stuff from large peaks if their
                 # boundsare not completely tight...
@@ -63,9 +63,8 @@ class FindSmallPeaks(plugin.TransformPlugin):
 
                 for oc in ocs:
                     # Focus only on the part of the occurrence inside the free region (superfluous as long as strict=True)
-                    # Remember: intervaltree uses half-open intervals, stop is the first index outside
                     start = max(region_left, oc.left)
-                    stop = min(region_right + 1, oc.right)
+                    stop = min(region_right, oc.right)
                     channel = oc.channel
 
                     # Don't consider channels from other detectors
@@ -77,9 +76,9 @@ class FindSmallPeaks(plugin.TransformPlugin):
                         continue
 
                     # Retrieve the waveform from pmt_waveforms
-                    w = event.pmt_waveforms[channel, start:stop]
+                    w = event.pmt_waveforms[channel, start : stop + 1]
 
-                    # Keep a copy, so we can filter w if needed:
+                    # Keep the unfiltered waveform in origw
                     origw = w
 
                     # Apply the filter, if user wants to
@@ -127,16 +126,21 @@ class FindSmallPeaks(plugin.TransformPlugin):
                     # Store the found peaks in the datastructure
                     peaks = []
                     for p in raw_peaks:
+                        # TODO: Hmzz.. height is computed in unfiltered waveform, noise_level in filtered!
+                        # TODO: height is wrong if baseline is corrected...
+                        # TODO: are you sure it is ok to compute area in filtered waveform?
+                        max_idx = p[0] + np.argmax(origw[p[0]:p[1] + 1])
                         peaks.append(datastructure.ChannelPeak({
-                            # TODO: store occurrence index -- occurrences needs to be a better datastructure first
+                            # TODO: store occurrence index
                             'channel':             channel,
                             'left':                start + p[0],
-                            'index_of_maximum':    start + p[1],
-                            'right':               start + p[2],
+                            # Compute index_of_maximum in the original waveform: the filter introduces a phase shift
+                            'index_of_maximum':    start + max_idx,
+                            'right':               start + p[1],
                             # NB: area and max are computed in filtered waveform, because
                             # the sliding window filter will shift the peak shape a bit
-                            'area':                np.sum(w[p[0]:p[2]+1]),
-                            'height':              w[p[1]],
+                            'area':                np.sum(w[p[0]:p[1]+1]),
+                            'height':              origw[max_idx],
                             'noise_sigma':         noise_sigma,
                         }))
                     event.all_channel_peaks.extend(peaks)
@@ -148,9 +152,9 @@ class FindSmallPeaks(plugin.TransformPlugin):
                             plt.plot(w, drawstyle='steps', label='data')
                         else:
                             plt.plot(w, drawstyle='steps', label='data (filtered)')
-                            plt.plot(origw, drawstyle='steps', label='data (raw)')
+                            plt.plot(origw, drawstyle='steps', label='data (raw)', alpha=0.5, color='gray')
                         for p in raw_peaks:
-                            plt.axvspan(p[0]-1, p[2], color='red', alpha=0.5)
+                            plt.axvspan(p[0]-1, p[1], color='red', alpha=0.5)
                         plt.plot(noise_sigma * np.ones(len(w)), '--', label='1 sigma')
                         plt.plot(self.min_sigma * noise_sigma * np.ones(len(w)), '--', label='%s sigma' % self.min_sigma)
                         plt.legend()
@@ -185,12 +189,12 @@ class FindSmallPeaks(plugin.TransformPlugin):
             height = w[max_idx]
             if height < noise_sigma * self.min_sigma:
                 continue
-            peaks.append((left, max_idx, right))
+            peaks.append((left, right))
         return peaks
 
     def samples_without_peaks(self, w, peaks):
         """Return array of bools of same size as w, True if none of peaks live there"""
         not_in_peak = np.ones(len(w), dtype=np.bool)    # All True
         for p in peaks:
-            not_in_peak[p[0]:p[2] + 1] = False
+            not_in_peak[p[0]:p[1] + 1] = False
         return not_in_peak