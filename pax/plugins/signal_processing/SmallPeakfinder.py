XENON1T/pax

commit ffe6996a88168fc926a43c3997acf3f0a7b6ada3

Store noise sigma and baseline correction for occurrences

pax/plugins/signal_processing/SmallPeakfinder.py
@@ -77,6 +77,10 @@ class FindSmallPeaks(plugin.TransformPlugin):
                     if event.is_channel_bad[channel]:
                         continue
 
+                    # Don't consider dead channels
+                    if self.config['gains'][channel] == 0:
+                        continue
+
                     # Retrieve the waveform from channel_waveforms
                     w = event.channel_waveforms[channel, start: stop + 1]
 
@@ -91,6 +95,8 @@ class FindSmallPeaks(plugin.TransformPlugin):
                     noise_sigma = self.initial_noise_sigma
                     old_raw_peaks = []
                     pass_number = 0
+                    baseline_correction_delta = 0
+                    baseline_correction = 0
                     while True:
                         # Determine the peaks based on the noise level
                         # Can't just use w > self.min_sigma * noise_sigma here,
@@ -107,7 +113,9 @@ class FindSmallPeaks(plugin.TransformPlugin):
 
                         # Correct the baseline
                         # -- BuildWaveforms can get it wrong if there is a pe in the starting samples
-                        w -= w[self.samples_without_peaks(w, raw_peaks)].mean()
+                        baseline_correction_delta += w[self.samples_without_peaks(w, raw_peaks)].mean()
+                        w -= baseline_correction_delta
+                        baseline_correction += baseline_correction_delta
 
                         # Determine the new noise_sigma
                         noise_sigma = self.noise_determination(w[self.samples_without_peaks(w, raw_peaks)])
@@ -122,6 +130,11 @@ class FindSmallPeaks(plugin.TransformPlugin):
                             break
 
                         pass_number += 1
+
+                    # Update occurrence data
+                    oc.noise_sigma = noise_sigma
+                    oc.baseline_correction = baseline_correction
+                    oc.height = oc.height - baseline_correction
 
                     # Update the noise occurrence count
                     if len(raw_peaks) == 0:

pax/config/debug.ini
@@ -11,6 +11,13 @@ logging_level = 'DEBUG'
 output_name = 'debug_output'
 output_format = 'csv'
 write_every = 1
+# Also store occurrence data:
+fields_to_ignore = [#'occurrences',
+                    'sum_waveforms',
+                    'channel_waveforms',
+                    'all_channel_peaks',
+                    'raw_data',
+                   ]
 
 [SmallPeakfinder.FindSmallPeaks]
 make_diagnostic_plots_in = 'debug_smallpf_diagnostic_plots'

pax/datastructure.py
@@ -194,12 +194,23 @@ class Occurrence(Model):
     channel = 0
 
     #: Maximum amplitude (in pe/bin; float)
-    #: Will remain None if channel's gain is 0
-    height = 0.0
+    #: Will remain nan if channel's gain is 0
+    #: baseline_correction, if any, has been substracted
+    # TODO: may not be equal to actual occurrence height,
+    # baseline correction is computed on 2-sample filtered wv. :-(
+    height = float('nan')
 
-    #: Baseline (in ADC counts; float)
-    # TODO: in small peakfinding, an improved baseline estimate is computed -- maybe we should store that too?
-    digitizer_baseline_used = 0.0
+    #: Noise sigma for this occurrence
+    #: Will remain nan unless processed by smallpeakfinder
+    noise_sigma = float('nan')
+
+    #: Baseline (in ADC counts, but float!)
+    #: Will remain nan if channel's gain is 0
+    digitizer_baseline_used = float('nan')
+
+    #: Baseline correction computed by FindSmallpeaks (in pe/bin)
+    #: Will remain nan if channel is not processed by FindSmallPeaks
+    baseline_correction = float('nan')
 
     #: Raw wave data (in ADC counts, NOT pe/bin!; numpy array of int16)
     raw_data = np.array([], np.int16)

commit e0049db6d4d138449730bd5ddec61e74ef2533d3

FindSmallPeaks: Fix bug in baseline correction Bug introduced in ffe6996a88168fc926a43c3997acf3f0a7b6ada3

pax/plugins/signal_processing/SmallPeakfinder.py
@@ -114,7 +114,7 @@ class FindSmallPeaks(plugin.TransformPlugin):
 
                         # Correct the baseline
                         # -- BuildWaveforms can get it wrong if there is a pe in the starting samples
-                        baseline_correction_delta += w[self.samples_without_peaks(w, raw_peaks)].mean()
+                        baseline_correction_delta = w[self.samples_without_peaks(w, raw_peaks)].mean()
                         w -= baseline_correction_delta
                         baseline_correction += baseline_correction_delta
 