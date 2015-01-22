80dcfa781e9cccc707b7748160802544f486300a
Jelle Aalbers <jelleaalbers@gmail.com>
2015-01-22 23:40:45 +0100

    Bugfix: default values didn't get stored. This is bad in itself, but it can also crash WritePandas if a field is only occasionally written to. I don't much like the solution, but it fixes the bug.

--- pax/data_model.py
+++ pax/data_model.py
@@ -50,7 +50,25 @@ class Model(object):
     def get_fields_data(self):
         """Iterator over (key, value) tuples of all user-specified fields
         """
-        return self.__dict__.items()
+        # self.__dict__.items() does not return default values set in class declaration
+        # Hence we need something more complicated
+        class_dict = self.__class__.__dict__
+        self_dict = self.__dict__
+        for field_name in class_dict.keys():
+            if field_name in self_dict:
+                # The instance has a value for this field: return it
+                yield (field_name, self_dict[field_name])
+            else:
+                # ... it doesnt. Should we return its value?
+                if field_name.startswith('_'):
+                    continue    # No, is internal
+                value_in_class = class_dict[field_name]
+                if callable(value_in_class):
+                    continue    # No, is a method
+                if isinstance(value_in_class, property):
+                    continue    # No, is a property
+                # Yes, yield the class-level value
+                yield (field_name, value_in_class)
 
     def to_dict(self, json_compatible=False):
         result = {}
