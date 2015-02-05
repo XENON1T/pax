d5d3993eeb37290d090bd5bea03089052db22cb9
Christopher Tunnell <ctunnell@nikhef.nl>
2015-02-05 17:06:27 +0100

    ROOT test

---
+++ tests/test_root.py
@@ -0,0 +1,35 @@
+import unittest
+
+import ROOT
+import numpy as n
+
+class TestRoot(unittest.TestCase):
+
+    def test_root(self):
+        print("Writing a tree")
+
+        f = ROOT.TFile("tree.root", "recreate")
+        t = ROOT.TTree("name_of_tree", "tree title")
+
+
+        # create 1 dimensional float arrays (python's float datatype corresponds to c++ doubles)
+        # as fill variables
+        n = n.zeros(1, dtype=float)
+        u = n.zeros(1, dtype=float)
+
+        # create the branches and assign the fill-variables to them
+        t.Branch('normal', n, 'normal/D')
+        t.Branch('uniform', u, 'uniform/D')
+
+        # create some random numbers, fill them into the fill varibles and call Fill()
+        for i in xrange(10):
+            n[0] = ROOT.gRandom.Gaus()
+            u[0] = ROOT.gRandom.Uniform()
+            t.Fill()
+
+        # write the tree into the output file and close the file
+        f.Write()
+        f.Close()
+
+if __name__ == '__main__':
+    unittest.main()
