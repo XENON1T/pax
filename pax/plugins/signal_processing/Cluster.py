Hitfinder: off-by-one errors, sanity checks Fixes last remnants of #101.

pax/plugins/signal_processing/Cluster.py
@@ -110,8 +110,8 @@ class ClusterPlugin(plugin.TransformPlugin):
                 # Are there any suspicious channels? If not, we are done.
                 self.log.debug(', '.join(['%s: %s' % (ch, round(bad_area_per_channel[ch], 1))
                                           for ch in np.where(bad_area_per_channel > 0)[0]]))
-                suspicious_channels = np.where(bad_area_per_channel
-                                               > self.config['bad_area_above_this_is_suspicious'])[0]
+                suspicious_channels = np.where(bad_area_per_channel >
+                                               self.config['bad_area_above_this_is_suspicious'])[0]
                 event.is_channel_suspicious[suspicious_channels] = True
                 if len(suspicious_channels) == 0:
                     break
@@ -147,6 +147,8 @@ class ClusterPlugin(plugin.TransformPlugin):
                 # Delete rejected hits from hits_to_cluster, then redo clustering
                 # The rejected hits will remain in event.all_channel_peaks, of course
                 hits_to_cluster = [h for h in hits_to_cluster if not h.is_rejected]
+
+                break
 
             # Add the peaks to the datastructure
             event.peaks.extend(peaks)

pax/plugins/signal_processing/HitFinder.py
@@ -31,16 +31,17 @@ class FindHits(plugin.TransformPlugin):
         # Conversion factor: multiply by this to convert from ADC counts above baseline -> electrons
         # Still has to be divided by PMT gain to go to photo-electrons (done below)
         self.adc_to_e = c['sample_duration'] * c['digitizer_voltage_range'] / (
-            2 ** (c['digitizer_bits'])
-            * c['pmt_circuit_load_resistor']
-            * c['external_amplification']
-            * units.electron_charge)
+            2 ** (c['digitizer_bits']) *
+            c['pmt_circuit_load_resistor'] *
+            c['external_amplification'] *
+            units.electron_charge)
 
     def transform_event(self, event):
         # Allocate numpy arrays to hold numba peakfinder results
-        hits_buffer = -1 * np.ones((self.max_hits_per_pulse, 2), dtype=np.int64)
-        argmaxes = np.zeros(self.max_hits_per_pulse, dtype=np.int64)
-        areas = np.zeros(self.max_hits_per_pulse)
+        # -42 is a placeholder for values that should never be displayed
+        hits_buffer = -42 * np.ones((self.max_hits_per_pulse, 2), dtype=np.int64)
+        argmaxes = -42 * np.zeros(self.max_hits_per_pulse, dtype=np.int64)
+        areas = -42 * np.ones(self.max_hits_per_pulse)
 
         for pulse_i, pulse in enumerate(event.occurrences):
             start = pulse.left
@@ -56,16 +57,18 @@ class FindHits(plugin.TransformPlugin):
 
             # Call the numba hit finder -- see its documentation below
             # Results stored in hits_buffer and extra_results; declared outside loop, see above
-            n_hits_found, baseline, noise_sigma = self._find_peaks(
+            n_hits_found, baseline, noise_sigma, passes_used = self._find_peaks(
                 w, self.min_sigma, self.noise_sigma_guess, self.max_passes, self.initial_baseline_samples,
                 hits_buffer)
 
-            if n_hits_found == len(hits_buffer) - 1:
-                self.log.warning("Occurrence %s-%s in channel %s has more than %s hits!"
-                                 "Probably the noise is unusually high. Further hits "
-                                 "have been ignored." % (start, stop, channel,
-                                                         self.max_hits_per_pulse))
-                n_hits_found = len(hits_buffer)
+            if n_hits_found >= self.max_hits_per_pulse:
+                self.log.warning("Pulse %s-%s in channel %s has more than %s hits!"
+                                 "This usually indicates a zero-length encoding breakdown after a very large S2."
+                                 "Further hits have been ignored." % (start, stop, channel, self.max_hits_per_pulse))
+
+            if passes_used >= self.max_passes:
+                self.log.debug("Hit finding in pulse %d-%d in channel %d did not converge after %d passes." % (
+                    start, stop, channel, passes_used))
 
             # If no hits were found, this is a noise pulse: update the noise pulse count
             if n_hits_found == 0:
@@ -89,16 +92,33 @@ class FindHits(plugin.TransformPlugin):
             # Convert area, noise_sigma and height from adc counts -> pe
             peaks = []
             adc_to_pe = self.adc_to_e / self.config['gains'][channel]
-            for i, p in enumerate(hits_found):
-                max_idx = p[0] + argmaxes[i]
+            for i, hit in enumerate(hits_found):
+
+                # Do sanity checks
+                area = areas[i] * adc_to_pe
+                height = w[hit[0] + argmaxes[i]] * adc_to_pe
+                left = start + hit[0]
+                right = start + hit[1]
+                max_idx = start + hit[0] + argmaxes[i]
+                noise_sigma_pe = noise_sigma * adc_to_pe
+                if not (0 <= left <= max_idx <= right) or not (0 <= self.min_sigma * noise_sigma_pe <= height <= area):
+                    raise RuntimeError("You found a hitfinder bug!\n"
+                                       "Current hit %d-%d-%d, in event %s, channel %s, pulse %s.\n"
+                                       "Indices in pulse: %s-%s-%s. Pulse bounds: %d-%d.\n"
+                                       "Height is %s, noise sigma is %s, dynamic threshold at %s; Area is %d.\n"
+                                       "Please tell Jelle!" % (
+                                           left, max_idx, right, event.event_number, channel, pulse_i,
+                                           hit[0], hit[0] + argmaxes[i], hit[1], start, stop,
+                                           height, noise_sigma_pe, self.min_sigma * noise_sigma_pe, area))
+
                 event.all_channel_peaks.append(datastructure.ChannelPeak({
                     'channel':             channel,
-                    'left':                start + p[0],
-                    'index_of_maximum':    start + max_idx,
-                    'right':               start + p[1],
-                    'area':                areas[i] * adc_to_pe,
-                    'height':              w[max_idx] * adc_to_pe,
-                    'noise_sigma':         noise_sigma * adc_to_pe,
+                    'left':                left,
+                    'index_of_maximum':    max_idx,
+                    'right':               right,
+                    'area':                area,
+                    'height':              height,
+                    'noise_sigma':         noise_sigma_pe,
                     'found_in_pulse':      pulse_i,
                 }))
 
@@ -108,8 +128,8 @@ class FindHits(plugin.TransformPlugin):
                self.make_diagnostic_plots == 'no peaks' and not len(peaks):
                 plt.figure()
                 plt.plot(w, drawstyle='steps', label='data')
-                for p in hits_found:
-                    plt.axvspan(p[0] - 1, p[1], color='red', alpha=0.5)
+                for hit in hits_found:
+                    plt.axvspan(hit[0] - 1, hit[1], color='red', alpha=0.5)
                 plt.plot(noise_sigma * np.ones(len(w)), '--', label='1 sigma')
                 plt.plot(self.min_sigma * noise_sigma * np.ones(len(w)),
                          '--', label='%s sigma' % self.min_sigma)
@@ -144,10 +164,10 @@ class FindHits(plugin.TransformPlugin):
             areas[peak_i] = current_area
 
     @staticmethod
-    @numba.jit(numba.typeof((1.0, 2.0, 3.0))(
+    @numba.jit(numba.typeof((1.0, 2.0, 3.0, 4.0))(
                numba.float64[:], numba.float64, numba.float64, numba.int64, numba.int64,
                numba.int64[:, :]), nopython=True)
-    def _find_peaks(w, threshold_sigmas, noise_sigma, max_passes, initial_baseline_samples, raw_peaks):
+    def _find_peaks(w, threshold_sigmas, noise_sigma, max_passes, initial_baseline_samples, hits_buffer):
         """Fills raw_peaks with left, right indices of raw_peaks > bound_threshold which exceed threshold somewhere
          raw_peaks: numpy () of [-1,-1] lists, will be filled by function.
         BE CAREFUL -- Will modify w IN PLACEto do baseline correction
@@ -179,7 +199,7 @@ class FindHits(plugin.TransformPlugin):
             in_candidate_interval = False
             current_interval_passed_test = False
             current_peak = 0
-            max_peaks = len(raw_peaks) - 1
+            max_n_peaks = len(hits_buffer)      # First index which is outside hits buffer = max # of peaks to find
             max_idx = len(w) - 1
             current_candidate_interval_start = -1
             threshold = noise_sigma * threshold_sigmas
@@ -203,6 +223,7 @@ class FindHits(plugin.TransformPlugin):
                         current_interval_passed_test = True
 
                     if x < bound_threshold or i == max_idx:
+
                         # End of candidate interval
                         in_candidate_interval = False
 
@@ -210,26 +231,25 @@ class FindHits(plugin.TransformPlugin):
                             # We've found a new peak!
 
                             # The interval ended just before this index
-                            # unless, of course, this is the last index
-                            itv_end = i if i == max_idx else i-1
+                            # unless, of course, we ended ONLY BECAUSE this is the last index
+                            itv_end = i-1 if x < bound_threshold else i
 
                             # Add to raw_peaks, check if something has changed
-                            if itv_end != raw_peaks[current_peak, 1] \
-                                    or current_candidate_interval_start != raw_peaks[current_peak, 0]:
-                                raw_peaks[current_peak, 0] = current_candidate_interval_start
-                                raw_peaks[current_peak, 1] = itv_end
+                            if itv_end != hits_buffer[current_peak, 1] \
+                                    or current_candidate_interval_start != hits_buffer[current_peak, 0]:
+                                hits_buffer[current_peak, 0] = current_candidate_interval_start
+                                hits_buffer[current_peak, 1] = itv_end
                                 has_changed = True
-
-                            # Check if we've reached the maximum # of peaks
-                            # If we found more peaks than we have room in our result array,
-                            # stop peakfinding immediately
-                            if current_peak == max_peaks:
-                                in_candidate_interval = False
-                                break
 
                             # Prepare for the next peak
                             current_peak += 1
                             current_interval_passed_test = False
+
+                            # Check if we've reached the maximum # of peaks
+                            # If we found more peaks than we have room in our result array,
+                            # stop peakfinding immediately
+                            if current_peak == max_n_peaks:
+                                break
 
             n_peaks_found = current_peak
 
@@ -253,13 +273,13 @@ class FindHits(plugin.TransformPlugin):
             for i, x in enumerate(w):
 
                 if currently_in_peak:
-                    if i > raw_peaks[current_peak, 1]:
+                    if i > hits_buffer[current_peak, 1]:
                         current_peak += 1
                         currently_in_peak = False
 
                 # NOT else, currently_in_peak may have changed!
                 if not currently_in_peak:
-                    if current_peak < n_peaks_found and i == raw_peaks[current_peak, 0]:
+                    if current_peak < n_peaks_found and i == hits_buffer[current_peak, 0]:
                         currently_in_peak = True
                     else:
                         # Update n, mean
@@ -285,13 +305,13 @@ class FindHits(plugin.TransformPlugin):
             for i, x in enumerate(w):
 
                 if currently_in_peak:
-                    if i > raw_peaks[current_peak, 1]:
+                    if i > hits_buffer[current_peak, 1]:
                         current_peak += 1
                         currently_in_peak = False
 
                 # NOT else, currently_in_peak may have changed!
                 if not currently_in_peak:
-                    if current_peak < n_peaks_found and i == raw_peaks[current_peak, 0]:
+                    if current_peak < n_peaks_found and i == hits_buffer[current_peak, 0]:
                         currently_in_peak = True
                     else:
                         if x < 0:
@@ -302,6 +322,6 @@ class FindHits(plugin.TransformPlugin):
             has_changed = False
             pass_number += 1
 
-        # Return number of peaks found, put rest of info in extra_results
-        # Convert n_peaks_found to float, if you keep it int, it will sometimes be int32, sometimes int64
-        return float(n_peaks_found), baseline, noise_sigma
+        # Return number of peaks found, baseline, noise sigma, and number of passes used
+        # Convert ints to float, if you keep it int, it will sometimes be int32, sometimes int64 => numba crashes
+        return float(n_peaks_found), baseline, noise_sigma, float(pass_number + 1)