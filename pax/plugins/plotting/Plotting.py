Tests, flake8. Also fix noise computation / baseline computation order issue

pax/plugins/plotting/Plotting.py
@@ -14,6 +14,7 @@ import numpy as np
 import time
 from pax import plugin, units
 
+
 class PlotBase(plugin.OutputPlugin):
 
     def startup(self):
@@ -380,11 +381,11 @@ class PlotChannelWaveforms2D(PlotBase):
                 10 * min(10, hit.area),                                # Size
                 (0.2 if hit.is_rejected else 1.0),                     # Alpha
             ])
-        rgba_colors = np.zeros((len(result),4))
+        rgba_colors = np.zeros((len(result), 4))
         result = np.array(result).T
-        rgba_colors[:,0] = result[2]
-        rgba_colors[:,2] = 1 - result[2]
-        rgba_colors[:,3] = result[4]
+        rgba_colors[:, 0] = result[2]
+        rgba_colors[:, 2] = 1 - result[2]
+        rgba_colors[:, 3] = result[4]
         plt.scatter(result[0], result[1], c=rgba_colors, s=result[3], lw=0)
 
         # Plot the bottom/top/veto boundaries

tests/test_checkpulses.py
@@ -5,7 +5,7 @@ import numpy as np
 from pax import core, datastructure, exceptions
 
 
-class TestBuildWaveforms(unittest.TestCase):
+class TestCheckPulses(unittest.TestCase):
 
     def setUp(self):  # noqa
         self.pax = core.Processor(config_names='XENON100',
@@ -13,10 +13,10 @@ class TestBuildWaveforms(unittest.TestCase):
                                   config_dict={
                                       'pax': {
                                           'plugin_group_names': ['test'],
-                                          'test':               'BuildWaveforms.BuildWaveforms'},
-                                      'BuildWaveforms.BuildWaveforms': {
-                                          'truncate_occurrences_partially_outside': False}})
-        self.plugin = self.pax.get_plugin_by_name('BuildWaveforms')
+                                          'test':               'CheckPulses.CheckBounds'},
+                                      'CheckPulses.CheckBounds': {
+                                          'truncate_occurrences_partially_outside': True}})
+        self.plugin = self.pax.get_plugin_by_name('CheckBounds')
         self.baseline = self.pax.config['DEFAULT']['digitizer_baseline']
 
     def make_single_occurrence_event(self, **kwargs):
@@ -30,13 +30,11 @@ class TestBuildWaveforms(unittest.TestCase):
         return event
 
     def test_basic_occurrence(self):
-
         event = self.make_single_occurrence_event(
             channel=1,
             left=1,
             raw_data=np.ones(20, dtype=np.int16) * self.baseline)
-        event = self.plugin.transform_event(event)
-        self.assertTrue(len(event.sum_waveforms) > 0)
+        self.plugin.transform_event(event)
 
     def test_strange_occurrences(self):
         # One sample at end
@@ -44,28 +42,58 @@ class TestBuildWaveforms(unittest.TestCase):
             channel=1,
             left=99,
             raw_data=np.ones(1, dtype=np.int16) * self.baseline)
-        event = self.plugin.transform_event(event)
-        self.assertTrue(len(event.sum_waveforms) > 0)
+        self.plugin.transform_event(event)
 
         # Occurrence fills event
         event = self.make_single_occurrence_event(
             channel=1,
             left=0,
             raw_data=np.ones(100, dtype=np.int16) * self.baseline)
-        event = self.plugin.transform_event(event)
-        self.assertTrue(len(event.sum_waveforms) > 0)
+        self.plugin.transform_event(event)
 
     def test_occurrences_outside_event(self):
-        # TODO: Also add tests for truncation itself, in case it is allowed in
-        # the config
 
+        # Occ starts to early
         event = self.make_single_occurrence_event(
             channel=1,
             left=-5,
             raw_data=np.ones(20, dtype=np.int16) * self.baseline)
-        self.assertRaises(exceptions.OccurrenceBeyondEventError,
-                          self.plugin.transform_event, event)
+        event = self.plugin.transform_event(event)
+        oc = event.occurrences[0]
+        self.assertEqual(oc.left, 0)
+        self.assertEqual(oc.right, 20-1-5)
 
+        # Occ starts to early
+        event = self.make_single_occurrence_event(
+            channel=1,
+            left=-1,
+            raw_data=np.ones(20, dtype=np.int16) * self.baseline)
+        event = self.plugin.transform_event(event)
+        oc = event.occurrences[0]
+        self.assertEqual(oc.left, 0)
+        self.assertEqual(oc.right, 20-1-1)
+
+        # Occ overhangs
+        event = self.make_single_occurrence_event(
+            channel=1,
+            left=90,
+            raw_data=np.ones(20, dtype=np.int16) * self.baseline)
+        event = self.plugin.transform_event(event)
+        oc = event.occurrences[0]
+        self.assertEqual(oc.left, 90)
+        self.assertEqual(oc.right, 99)
+
+        # Occ starts too early AND overhangs
+        event = self.make_single_occurrence_event(
+            channel=1,
+            left=-5,
+            raw_data=np.ones(200, dtype=np.int16) * self.baseline)
+        event = self.plugin.transform_event(event)
+        oc = event.occurrences[0]
+        self.assertEqual(oc.left, 0)
+        self.assertEqual(oc.right, 99)
+
+        # Occ entirely outside
         event = self.make_single_occurrence_event(
             channel=1,
             left=-5,
@@ -73,6 +101,7 @@ class TestBuildWaveforms(unittest.TestCase):
         self.assertRaises(exceptions.OccurrenceBeyondEventError,
                           self.plugin.transform_event, event)
 
+        # Occ entirely outside
         event = self.make_single_occurrence_event(
             channel=1,
             left=120,
@@ -80,16 +109,22 @@ class TestBuildWaveforms(unittest.TestCase):
         self.assertRaises(exceptions.OccurrenceBeyondEventError,
                           self.plugin.transform_event, event)
 
-        event = self.make_single_occurrence_event(
-            channel=1,
-            left=-5,
-            raw_data=np.ones(200, dtype=np.int16) * self.baseline)
-        self.assertRaises(exceptions.OccurrenceBeyondEventError,
-                          self.plugin.transform_event, event)
-
+        # Occ entirely outside
         event = self.make_single_occurrence_event(
             channel=1,
             left=100,
             raw_data=np.ones(1, dtype=np.int16) * self.baseline)
         self.assertRaises(exceptions.OccurrenceBeyondEventError,
                           self.plugin.transform_event, event)
+
+        # Occ entirely outside
+        event = self.make_single_occurrence_event(
+            channel=1,
+            left=-1,
+            raw_data=np.ones(1, dtype=np.int16) * self.baseline)
+        self.assertRaises(exceptions.OccurrenceBeyondEventError,
+                          self.plugin.transform_event, event)
+
+
+if __name__ == '__main__':
+    unittest.main()