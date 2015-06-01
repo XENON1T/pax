475803d1523a24e315dcc0b5117980978a8f1ebd
pelssers <bartpelssers@gmail.com>
2015-06-01 11:03:30 +0200

    enabled binaries for travis

--- travis/install-ROOT-binary.sh
+++ travis/install-ROOT-binary.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-conda remove system
 
-time wget -nv --no-check-certificate http://www.nikhef.nl/~bartp/root_v${ROOT}_binaries.tar.gz
+if [ -z "$ROOT" ]; then
+    echo "ROOT version not set. Making guess"
+    export ROOT=6.03.04
+fi
+
+if [ "${TRAVIS_OS_NAME}" == "linux" ]
+then
+    conda remove system
+fi
+
+time wget -nv --no-check-certificate http://www.nikhef.nl/~bartp/root_v${ROOT}_linux_binaries.tar.gz
 tar xfz root_v${ROOT}_binaries.tar.gz
 cd root*
 
 source bin/thisroot.sh
 
+export LD_LIBRARY_PATH=`python3.4-config --exec-prefix`/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$LD_LIBRARY_PATH
+
 cd ..
+
+if [ "${TRAVIS_OS_NAME}" == "linux" ]
+then
+    conda install system
+fi
