6a564d5f05b27a08a9259297dc52d9991c9469eb
Axel Buß <a_buss09@uni-muenster.de>
2015-06-17 14:40:00 +0200

    added unlikely units aka Gs, nHz etc. to pass test. Later test file needs to be adapted if those useless unit should be removed

--- pax/units.py
+++ pax/units.py
@@ -40,6 +40,7 @@ mm = 10 ** (-3) * m
 ms = 10 ** (-3) * s
 mK = 10 ** (-3) * K
 mC = 10 ** (-3) * C
+meV = 10 ** (-3) * eV
 
 mHz = 10 ** (-3) * Hz
 mJ = 10 ** (-3) * J
@@ -56,6 +57,7 @@ um = 10 ** (-6) * m
 us = 10 ** (-6) * s
 uK = 10 ** (-6) * K
 uC = 10 ** (-6) * C
+ueV = 10 ** (-6) * eV
 
 uHz = 10 ** (-6) * Hz
 uJ = 10 ** (-6) * J
@@ -72,38 +74,71 @@ nm = 10 ** (-9) * m
 ns = 10 ** (-9) * s
 nK = 10 ** (-9) * K
 nC = 10 ** (-9) * C
+neV = 10 ** (-9) * eV
 
+nHz = 10 ** (-9) * Hz
 nJ = 10 ** (-9) * J
 ug = 10 ** (-9) * kg
 nV = 10 ** (-9) * V
+nA = 10 ** (-9) * A
+nN = 10 ** (-9) * N
+nPa = 10 ** (-9) * Pa
+nbar = 10 ** (-9) * bar
+nOhm = 10 ** (-9) * Ohm
 
 # 10 ^ 3 base units
 km = 10 ** 3 * m
-kJ = 10 ** 3 * J
-kV = 10 ** 3 * V
+ks = 10 ** 3 * s
+kK = 10 ** 3 * K
+kC = 10 ** 3 * C
+keV = 10 ** 3 * eV
+
 kHz = 10 ** 3 * Hz
+kJ = 10 ** 3 * J
+Mg = 10 ** 3 * kg
+kV = 10 ** 3 * V
 kA = 10 ** 3 * A
 kN = 10 ** 3 * N
 kOhm = 10 ** 3 * Ohm
 kPa = 10 ** 3 * Pa
-keV = 10 ** 3 * eV
+
 
 # 10 ^ 6 base units
-MJ = 10 ** 6 * J
-MV = 10 ** 6 * V
+Mm = 10 ** 6 * m
+Ms = 10 ** 6 * s
+MK = 10 ** 6 * K
+MC = 10 ** 6 * C
+MeV = 10 ** 6 * eV
+
 MHz = 10 ** 6 * Hz
+MJ = 10 ** 6 * J
+Gg = 10 ** 6 * kg
+MV = 10 ** 6 * V
+MA = 10 ** 6 * A
 MN = 10 ** 6 * N
 MOhm = 10 ** 6 * Ohm
 MPa = 10 ** 6 * Pa
-MeV = 10 ** 6 * eV
+
 
 # 10 ^ 9 base units
-GJ = 10 ** 9 * J
+Gm = 10 ** 9 * m
+Gs = 10 ** 9 * s
+GK = 10 ** 9 * K
+GC = 10 ** 9 * C
+GeV = 10 ** 9 * eV
+
 GHz = 10 ** 9 * Hz
+GJ = 10 ** 9 * J
+GV = 10 ** 9 * V
+GA = 10 ** 9 * A
+GN = 10 ** 9 * N
+GOhm = 10 ** 9 * Ohm
 GPa = 10 ** 9 * Pa
+
 
 # other units
 cm = 10 ** (-2) * m
+ng = 10 ** (-12) * kg
 # Townsend (unit for reduced electric field)
 Td = 10 ** (-17) * V / cm ** 2  # noqa
 
