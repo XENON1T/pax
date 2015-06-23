00938ab81618371212180200f1e4857b03d21e41
Axel Buß <a_buss09@uni-muenster.de>
2015-06-23 16:51:02 +0200

    started creating a waveform viewer

---
+++ examples/muenster/ShowWaveforms.py
@@ -0,0 +1,11 @@
+""""
+This class will plot all waveforms that have been recorded. It reads the created pulses for each event
+and plot the waveforms in a single plot
+"""
+
+import pax.plugins.plotting.Plotting
+
+
+class ShowWaveforms(pax.PlotBase):
+    def PlotAllChannels(self, event):
+        pass
