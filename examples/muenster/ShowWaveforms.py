04d1e50964cf6228d6fb0513e812f99eab8398da
Axel Buß <a_buss09@uni-muenster.de>
2015-06-23 17:10:31 +0200

    started creating a waveform viewer

--- examples/muenster/ShowWaveforms.py
+++ examples/muenster/ShowWaveforms.py
@@ -3,9 +3,15 @@ This class will plot all waveforms that have been recorded. It reads the created
 and plot the waveforms in a single plot
 """
 
+import matplotlib as plt
+
 import pax.plugins.plotting.Plotting
 
 
 class ShowWaveforms(pax.PlotBase):
     def PlotAllChannels(self, event):
-        pass
+        fig, ax = plt.subplots(nrows=7, ncols=2, sharex=True, sharey=True, squeeze=False, figsize=(12, 12))
+        plt.subplots_adjust(hspace=0, wspace=0.05)
+        for xi in range(7):
+            for yi in range(2):
+                ax[xi][yi].plot(range(10), range(10))
