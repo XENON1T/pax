f61942529c35bbb5c383fd1d2c46f20c48a44689
Jelle Aalbers <jelleaalbers@gmail.com>
2015-10-20 20:24:00 +0200

    Lint

--- pax/simulation.py
+++ pax/simulation.py
@@ -540,7 +540,6 @@ class Simulator(object):
         if not self.s1_patterns:
             return self.randomize_photons_over_channels(n_photons, self.config['channels_in_detector']['tpc'])
         # TODO: compensate for S2 width & drift velocity increase after gate (both ~us effects though, not important)
-        drift_time = z / self.config['drift_velocity_liquid']
         return self.distribute_photons_by_pattern(n_photons, self.s1_patterns, (x, y, z))
 
     def distribute_photons_by_pattern(self, n_photons, pattern_fitter, coordinate_tuple):
