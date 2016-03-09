4525c318f02ff93dd98b21a650f21cd5ffaee35c
pelssers <bartpelssers@gmail.com>
2016-03-09 14:03:37 +0100

    Lower precision to 7 decimal digits in assert

--- tests/test_posrec_neuralnet_1T.py
+++ tests/test_posrec_neuralnet_1T.py
@@ -50,8 +50,8 @@ class TestPosRecNeuralNet(unittest.TestCase):
         self.assertEqual(len(e.peaks[0].reconstructed_positions), 1)
         rp = e.peaks[0].reconstructed_positions[0]
         self.assertEqual(rp.algorithm, self.plugin.name)
-        self.assertEqual(rp.x, 10.258076101568305)
-        self.assertEqual(rp.y, 17.785535721706857)
+        self.assertAlmostEqual(rp.x, 10.258076101568305, places=7)
+        self.assertAlmostEqual(rp.y, 17.785535721706857, places=7)
 
 
 if __name__ == '__main__':
