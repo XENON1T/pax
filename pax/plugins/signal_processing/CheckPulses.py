35e02c045b14c5c0baccd321d189e0d047f8dd29
Jelle Aalbers <jelleaalbers@gmail.com>
2016-05-01 12:43:27 +0200

    Debug message is sufficient for pulse partially out of bounds

--- pax/plugins/signal_processing/CheckPulses.py
+++ pax/plugins/signal_processing/CheckPulses.py
@@ -91,6 +91,8 @@ class CheckBounds(plugin.TransformPlugin):
 
             if start_index < 0 or end_index < 0 or overhang > 0:
 
+                # If completely outside, mark as to-be-ignored with warning, or give error, according to config
+                # to-be-ignored pulses are removed later (can't do here as we're iterating over pulses)
                 if overhang >= length or start_index <= -length or end_index < 0:
                     text = ('Pulse %s in channel %s (%s-%s) is entirely outside '
                             'event bounds (%s-%s)! See issue #43.' % (pulse_i, channel, start_index, end_index,
@@ -104,14 +106,14 @@ class CheckBounds(plugin.TransformPlugin):
 
                 pulse_wave = pulse.raw_data
 
-                # If partially outside, truncate with warning, or give error, according to config
+                # If partially outside, truncate with debug message, or give error, according to config
                 message = 'Pulse %s in channel %s (%s-%s) is partially outside ' \
                           'event bounds (%s-%s). See issue #43' % (
                               pulse_i, channel, start_index, end_index, 0, event_length - 1)
 
                 if not self.truncate_pulses_partially_outside:
                     raise exceptions.PulseBeyondEventError(message)
-                self.log.warning(message)
+                self.log.debug(message)
 
                 # Truncate the pulse. Remember start_index < 0!
                 if start_index < 0:
@@ -121,14 +123,13 @@ class CheckBounds(plugin.TransformPlugin):
                     pulse_wave = pulse_wave[:-overhang]
                     end_index = event_length - 1
 
-                # Update the pulse data, so hit finder won't look at old un-truncated occ
-                event.pulses[pulse_i] = pulse = datastructure.Pulse(
-                    left=start_index,
-                    right=end_index,
-                    channel=channel,
-                    raw_data=pulse_wave
-                )
+                # Update the pulse data, so hit finder won't look at old un-truncated pulse
+                event.pulses[pulse_i] = datastructure.Pulse(left=start_index,
+                                                            right=end_index,
+                                                            channel=channel,
+                                                            raw_data=pulse_wave)
 
+        # Remove the to-be-ignored-pulses
         event.pulses = [p for p_i, p in enumerate(event.pulses) if p_i not in pulses_to_ignore]
 
         return event
