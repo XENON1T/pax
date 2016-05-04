d145a3435f7f2189af04ab6fa328c63a767fdcaf
Dominick Cichon <Dominick.Cichon@mpi-hd.mpg.de>
2016-05-04 16:35:20 +0200

    Fixed a case of the FaX physics backend trying to use a non-existing logger.

--- pax/simulation.py
+++ pax/simulation.py
@@ -262,10 +262,10 @@ class Simulator(object):
                         "Generated pulse is %s samples long, can't be inserted between %s and %s" % (
                             len(generated_pulse), left_index, righter_index))
                 elif left_index < 0:
-                    self.log.debug("Invalid left index %s: can't be negative" % left_index)
+                    log.debug("Invalid left index %s: can't be negative" % left_index)
                     continue
                 elif righter_index >= len(current_wave):
-                    self.log.debug("Invalid right index %s: can't be longer than length of wave (%s)!" % (
+                    log.debug("Invalid right index %s: can't be longer than length of wave (%s)!" % (
                         righter_index, len(current_wave)))
                     continue
 
