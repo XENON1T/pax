f3aa7c703a12a8e25e9fe320878f6562dd5acb80
Jelle Aalbers <jelleaalbers@gmail.com>
2016-05-18 18:46:19 +0200

    Rough XENON1T classification
    Below 50pe everything is marked as "unknown'. Otherwise this needed very
    little tuning (slightly relaxed the S1 width bounds).

--- pax/plugins/peak_processing/ClassifyPeaks.py
+++ pax/plugins/peak_processing/ClassifyPeaks.py
@@ -1,6 +1,29 @@
 from pax import plugin, units
 
 
+class AdHocClassification1T(plugin.TransformPlugin):
+
+    def transform_event(self, event):
+
+        for peak in event.peaks:
+            # Don't work on noise and lone_hit
+            if peak.type in ('noise', 'lone_hit'):
+                continue
+
+            width = peak.range_area_decile[5]
+
+            if peak.area > 50:
+                # We don't have to worry about single electrons anymore
+                if width < 175 * units.ns:
+                    peak.type = 's1'
+                elif width > 250 * units.ns:
+                    peak.type = 's2'
+            else:
+                peak.type = 'unknown'
+
+        return event
+
+
 class AdHocClassification(plugin.TransformPlugin):
 
     def transform_event(self, event):
