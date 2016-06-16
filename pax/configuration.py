bfec178ce5c3997c367294b0668dfbb19a3750b0
Jelle Aalbers <jelleaalbers@gmail.com>
2016-06-16 16:20:29 +0200

    Fix good

--- pax/configuration.py
+++ pax/configuration.py
@@ -142,4 +142,4 @@ def fix_sections_from_mongo(config):
     """Returns configuration with | replaced with . in section keys.
     Needed because . in field names has special meaning in MongoDB
     """
-    return {k.replace('|', '.'): v for k, v in config}
+    return {k.replace('|', '.'): v for k, v in config.items()}
