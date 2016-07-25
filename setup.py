3b58f780ad2d41bc5b1720457963402f921a852a
Christopher Tunnell <ctunnell@nikhef.nl>
2016-07-25 21:35:24 +0200

    Tweak setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ test_requirements = requirements + ['flake8',
 setup(
     name='pax',
     version='5.3.3',
-    description='PAX is the raw data processor for the XENON1T experiment, with support for other LXe TPCs.',
+    description='PAX is the raw data processor for LXe TPCs.',
     long_description=readme + '\n\n' + history,
     author='Christopher Tunnell and Jelle Aalbers for the XENON1T collaboration',
     author_email='ctunnell@nikhef.nl',
