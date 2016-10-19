402eb836d20859927e000f0b69018d1926bd6e25
Jelle Aalbers <jelleaalbers@gmail.com>
2016-10-19 11:00:38 +0200

    Rename queue plugin file

--- pax/plugins/io/queue.py
+++ pax/plugins/io/queue.py
@@ -10,23 +10,24 @@ class SharedMemoryQueuePlugin:
 
     def startup(self):
         self.queue = self.config['queue']
-        self.status = self.config['status']
+        self.status_magic_variable = self.config['status_magic_variable']
         # input_done for regular workers processing_done for output worker
         self.status_if_can_end = MP_STATUS[self.config.get('status_if_can_end', 'input_done')]
-        if self.config['ordered']:
-            self.block_heap = []
+        self.block_heap = []
 
     def check_crash(self):
+        current_status = self.get_status()
         if self.status.value == MP_STATUS['crashing']:
             exit('')
 
-    def push_to_queue(self, event_block, block_id):
+    def get_status(self):
+        return self.status_magic_variable.value
+
+    def set_status(self):
 
 
 
 class PullSharedMemoryQueue(plugin.InputPlugin, SharedMemoryQueuePlugin):
-
-
 
 
 
@@ -35,11 +36,11 @@ class PullSharedMemoryQueue(plugin.InputPlugin, SharedMemoryQueuePlugin):
         block_id = -1  # Current block id. Needed for ordered pulling.
 
         while True:
-            can_end = self.status_if_can_end
-
-            if self.config['ordered']:
-                # We have to ensure the event blocks are pulled out in order.
-                try:
+            current_status = self.get_status()
+            can_end = self.current_status == self.status_if_can_end
+            try:
+                if self.config['ordered']:
+                    # We have to ensure the event blocks are pulled out in order.
                     # If we don't have the block we want yet, keep fetching event blocks from the queue
                     # and push them onto the heap.
                     # Note: if one block takes much longer than the others, this would slurp all blocks
@@ -51,29 +52,20 @@ class PullSharedMemoryQueue(plugin.InputPlugin, SharedMemoryQueuePlugin):
                         self.log.debug("Earliest block: %d, looking for block %s" % (block_heap[0][0],
                                                                                      block_id + 1))
 
-                except queue.Empty:
-                    if can_end and not len(block_heap):
-                        # We're done!
-                        break
-                    # Queue is empty, but either we're not in the processing_done status, or we're waiting for a
-                    # block which hasn't arrived on the queue yet. Wait, then try the queue again.
-                    time.sleep(1)
-                    continue
+                    # If we get here, we have the event block we need sitting at the top of the heap
+                    block_id, event_block = heapq.heappop(block_heap)
 
-                # If we get here, we have the event block we need sitting at the top of the heap
-                block_id, event_block = heapq.heappop(block_heap)
-
-            else:
-                try:
+                else:
                     block_id, event_block = self.queue.get(block=True, timeout=1)
-                except queue.Empty:
-                    if can_end:
-                        # We're done, no more events!
-                        break
-                    # Queue is empty: wait for more data, then check can_end again
-                    self.log.debug("Found empty queue, can_end is %s, status is %s" % (can_end, self.status.value))
-                    time.sleep(1)
-                    continue
+
+            except queue.Empty:
+                if can_end and not len(block_heap):
+                    # We're done, no more events!
+                    break
+                # Queue is empty: wait for more data, then check can_end again
+                self.log.debug("Found empty queue, can_end is %s, status is %s" % (can_end, current_status))
+                time.sleep(1)
+                continue
 
             try:
                 self.log.debug("%s now processing block %d" % (self.worker_id, block_id))
@@ -84,7 +76,7 @@ class PullSharedMemoryQueue(plugin.InputPlugin, SharedMemoryQueuePlugin):
 
             except Exception:
                 # Crash occurred during processing: notify everyone else, then die
-                self.status.value = MP_STATUS['crashing']
+                self.set_global_status(MP_STATUS['crashing'])
                 raise
 
 
