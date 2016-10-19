02bd6f101529009e446724d288e1b2945b2752df
Christopher Tunnell <ctunnell@nikhef.nl>
2016-10-19 12:37:56 +0200

    Do not create DynamoDB table

--- pax/plugins/io/AmazonAWS.py
+++ pax/plugins/io/AmazonAWS.py
@@ -11,35 +11,7 @@ class WriteDynamoDB(plugin.OutputPlugin):
 
     def startup(self):
         self.dynamodb = boto3.resource('dynamodb')
-        # Create the DynamoDB table.
-        self.table = self.dynamodb.create_table(TableName='processed',
-                                                KeySchema=[
-                                                    {
-                                                        'AttributeName': 'event_number',
-                                                        'KeyType': 'HASH'
-                                                    },
-                                                    {
-                                                        'AttributeName': 'dataset_name',
-                                                        'KeyType': 'RANGE'
-                                                    }
-                                                ],
-                                                AttributeDefinitions=[
-                                                    {
-                                                        'AttributeName': 'event_number',
-                                                        'AttributeType': 'N'
-                                                    },
-                                                    {
-                                                        'AttributeName': 'dataset_name',
-                                                        'AttributeType': 'S'
-                                                    },
-
-                                                ],
-                                                ProvisionedThroughput={
-                                                    'ReadCapacityUnits': 5,
-                                                    'WriteCapacityUnits': 5
-                                                }
-                                                )
-
+        
     def write_event(self, event):
         self.table.put_item(Item=event.to_dict(
             fields_to_ignore=self.config['fields_to_ignore']))
