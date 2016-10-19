9786a79619149f84c0b138b73927b3cb0e8ccb5c
Jelle Aalbers <jelleaalbers@gmail.com>
2016-10-19 11:00:48 +0200

    Continue queue plugin development

--- pax/plugins/io/queue.py
+++ pax/plugins/io/queue.py
@@ -6,30 +6,53 @@ from pax import plugin
 from pax.core import MP_STATUS
 
 
-class SharedMemoryQueuePlugin:
+
+class QueuePlugin:
+
+    def check_crash(self):
+        if self.get_status() == MP_STATUS['crashing']:
+            exit('')
+
+    def get_status(self):
+        raise NotImplementedError
+
+    def set_status(self):
+        raise NotImplementedError
+
+    def push_event_block(self, block_id, event_block):
+        raise NotImplementedError
+
+    def pull_event_block(self):
+        """Pull event block from queue, or raise queue.Empty if no events queued"""
+        raise NotImplementedError
+
+
+class SharedMemoryQueuePlugin(QueuePlugin):
 
     def startup(self):
         self.queue = self.config['queue']
         self.status_magic_variable = self.config['status_magic_variable']
         # input_done for regular workers processing_done for output worker
-        self.status_if_can_end = MP_STATUS[self.config.get('status_if_can_end', 'input_done')]
         self.block_heap = []
-
-    def check_crash(self):
-        current_status = self.get_status()
-        if self.status.value == MP_STATUS['crashing']:
-            exit('')
 
     def get_status(self):
         return self.status_magic_variable.value
 
-    def set_status(self):
+    def set_status(self, status_key):
+        self.status_magic_variable.value = MP_STATUS[status_key]
+
+    def push_event_block(self, block_id, events):
+        self.queue.put((block_id, events))
+
+    def pull_event_block(self):
+        self.queue.get(block=True, timeout=1)
 
 
 
-class PullSharedMemoryQueue(plugin.InputPlugin, SharedMemoryQueuePlugin):
+class QueuePullPlugin(plugin.InputPlugin, QueuePlugin):
 
-
+    def startup(self):
+        self.status_if_can_end = MP_STATUS[self.config.get('status_if_can_end', 'input_done')]
 
     def get_events(self):
         block_heap = self.block_heap
@@ -37,17 +60,15 @@ class PullSharedMemoryQueue(plugin.InputPlugin, SharedMemoryQueuePlugin):
 
         while True:
             current_status = self.get_status()
-            can_end = self.current_status == self.status_if_can_end
+            can_end = current_status == self.status_if_can_end
             try:
                 if self.config['ordered']:
                     # We have to ensure the event blocks are pulled out in order.
                     # If we don't have the block we want yet, keep fetching event blocks from the queue
-                    # and push them onto the heap.
-                    # Note: if one block takes much longer than the others, this would slurp all blocks
-                    # while waiting for the difficult one to come through, potentially exhausting your RAM...
+                    # and push them onto a heap.
                     while not (len(block_heap) and block_heap[0][0] == block_id + 1):
                         self.check_crash()
-                        heapq.heappush(block_heap, self.queue.get(block=True, timeout=1))
+                        heapq.heappush(block_heap, self.pull_event_block())
                         self.log.debug("Just got a block, heap is now %d blocks long" % len(block_heap))
                         self.log.debug("Earliest block: %d, looking for block %s" % (block_heap[0][0],
                                                                                      block_id + 1))
@@ -56,7 +77,7 @@ class PullSharedMemoryQueue(plugin.InputPlugin, SharedMemoryQueuePlugin):
                     block_id, event_block = heapq.heappop(block_heap)
 
                 else:
-                    block_id, event_block = self.queue.get(block=True, timeout=1)
+                    block_id, event_block = self.pull_event_block()
 
             except queue.Empty:
                 if can_end and not len(block_heap):
@@ -68,7 +89,7 @@ class PullSharedMemoryQueue(plugin.InputPlugin, SharedMemoryQueuePlugin):
                 continue
 
             try:
-                self.log.debug("%s now processing block %d" % (self.worker_id, block_id))
+                self.log.debug("Now processing block %d" % block_id)
                 for i, event in enumerate(event_block):
                     self.check_crash()
                     event.block_id = block_id
@@ -76,13 +97,17 @@ class PullSharedMemoryQueue(plugin.InputPlugin, SharedMemoryQueuePlugin):
 
             except Exception:
                 # Crash occurred during processing: notify everyone else, then die
-                self.set_global_status(MP_STATUS['crashing'])
+                self.set_status('crashing')
                 raise
 
 
-class PushSharedMemoryQueue(plugin.OutputPlugin, SharedMemoryQueuePlugin):
+class QueuePushPlugin(plugin.OutputPlugin, QueuePlugin):
 
-    def write(self, event):
-        # TODO: gather in blocks
+    def startup(self):
+        self.current_block = []
+        self.block_size = self.config['pax'].get('event_block_size', 10)
 
-        self.queque.put((event.block_id, event))
+    def write_event(self, event):
+        self.current_block.append(event)
+        if len(self.current_block) >= self.block_size:
+            self.queue.put((event.block_id, event))
