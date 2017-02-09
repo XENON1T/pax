Fix of negative computation of center of gravity for negative samples in hits-only sum waveform (#490)

pax/plugins/ZLE.py
@@ -14,7 +14,7 @@ class SoftwareZLE(plugin.TransformPlugin):
     Uses a separate debug setting, as need to show plots
     """
     debug = False
-    zle_intervals_buffer = -1 * np.ones((5000, 2), dtype=np.int64)
+    zle_intervals_buffer = -1 * np.ones((50000, 2), dtype=np.int64)
 
     def transform_event(self, event):
         new_pulses = []

pax/plugins/peak_processing/BasicProperties.py
@@ -86,7 +86,9 @@ class SumWaveformProperties(plugin.TransformPlugin):
                 peak.center_time = float('nan')
                 continue
             else:
-                peak.center_time = (peak.left + np.average(np.arange(len(w)), weights=w)) * dt
+                peak.center_time = (peak.left + np.average(np.arange(len(w)),
+                                                           weights=np.clip(w, 0, float('inf')))) * dt
+                # Cut out negative samples for computation of center of gravity
 
             # Index in peak waveform nearest to center of gravity (for sum-waveform alignment)
             cog_idx = int(round(peak.center_time / dt)) - peak.left