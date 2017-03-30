0b93b24b97a04b9d9977f1d69ee3c80d1eafc6f7
Christopher Tunnell <ctunnell@nikhef.nl>
2017-03-30 16:46:25 -0500

    Bump version: 6.6.2 → 6.6.3

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ test_requirements = requirements + ['flake8',
 
 setup(
     name='pax',
-    version='6.6.2',
+    version='6.6.3',
     description='PAX is the raw data processor for LXe TPCs.',
     long_description=readme + '\n\n' + history,
     author='Christopher Tunnell and Jelle Aalbers for the XENON1T collaboration',
