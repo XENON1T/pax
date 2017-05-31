2bbf2c444fd8db9c54fb81a314708b3acd0f389a
Fei Gao <feigao.ge@gmail.com>
2017-05-31 14:19:41 -0400

    classification update (#576)

--- pax/plugins/peak_processing/ClassifyPeaks.py
+++ pax/plugins/peak_processing/ClassifyPeaks.py
@@ -5,9 +5,11 @@ from scipy import interpolate
 class AdHocClassification1T(plugin.TransformPlugin):
 
     def startup(self):
-        self.s1_rise_time_bound = interpolate.interp1d([0, 10, 25, 100],
+        self.s1_rise_time_bound = interpolate.interp1d([0, 5, 10, 100],
                                                        [100, 100, 70, 70],
                                                        fill_value='extrapolate', kind='linear')
+        self.s1_rise_time_aft = interpolate.interp1d([0, 0.3, 0.4, 0.5, 0.70, 0.70, 1.0],
+                                                     [70, 70, 65, 60, 35, 0, 0], kind='linear')
 
     def transform_event(self, event):
 
@@ -21,8 +23,14 @@ class AdHocClassification1T(plugin.TransformPlugin):
                 if peak.tight_coincidence <= 2:
                     # Too few PMTs contributing, hard to distinguish from junk
                     peak.type = 'unknown'
-                else:
+                elif peak.area > 100 or peak.area < 5:
+                    # Apply single electron s2 cut only in 5 - 100 PE range, otherwise only rely on rise time
                     peak.type = 's1'
+                elif -peak.area_decile_from_midpoint[1] < self.s1_rise_time_aft(peak.area_fraction_top):
+                    # Rise time and AFT as multi-demensional discriminator
+                    peak.type = 's1'
+                else:
+                    peak.type = 's2'
             else:
                 # No fast rise
                 if peak.n_contributing_channels > 4:
