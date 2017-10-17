1012ce1e932b1690d69f344e1d0088c80988a64d
Fei Gao <feigao.ge@gmail.com>
2017-10-17 15:40:03 -0400

    Revert "tuning clustering for SR1 (#615)"
    This reverts commit f580b718f9e6516e6d0576874bb04fa72104467c.

--- pax/plugins/signal_processing/BuildPeaks.py
+++ pax/plugins/signal_processing/BuildPeaks.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy import interpolate
+
 from pax import plugin, dsputils
 from pax.plugins.peak_processing.BasicProperties import integrate_until_fraction
 
@@ -19,17 +19,8 @@ class GapSizeClustering(plugin.ClusteringPlugin):
         # Maximum gap inside other clusters
         self.gap_threshold = self.config['max_gap_size_in_cluster'] / self.dt
 
-        # Rise time bound
-        self.s1_rise_time_bound = interpolate.interp1d([0, 5, 10, 100],
-                                                       [80, 75, 70, 70],
-                                                       fill_value='extrapolate', kind='linear')
-
-        # Rise time vs AFT cut
-        self.s1_rise_time_aft = interpolate.interp1d([0, 0.4, 0.5, 0.6, 0.70, 0.70, 1.0],
-                                                     [70, 70, 68, 65, 60, 0, 0], kind='linear')
-
-        self.first_top_ch = np.min(np.array(self.config['channels_top']))
-        self.last_top_ch = np.max(np.array(self.config['channels_top']))
+        # Rise time threshold to mark S1 candidates
+        self.rise_time_threshold = self.config.get('rise_time_threshold', 80)
 
     @staticmethod
     def iterate_gap_clusters(hits, gap_threshold):
@@ -53,6 +44,9 @@ class GapSizeClustering(plugin.ClusteringPlugin):
             # First cluster into small clusters. Try to find S1 candidates among them, and set these apart
             s1_mask = np.zeros(len(hits), dtype=np.bool)    # True if hit is part of S1 candidate
             for l_i, r_i, h in self.iterate_gap_clusters(hits, self.s1_gap_threshold):
+                l = h['left_central'].min()
+                # center = np.sum(h['center'] * h['area']) / h['area'].sum() / self.dt
+                # rise_time = (center - l)
                 area_sum = np.sum(h['area'])
                 peak = self.build_peak(hits=h, detector=detector)
                 # use summed WF to calculate peak properties
@@ -66,22 +60,12 @@ class GapSizeClustering(plugin.ClusteringPlugin):
                 area_midpoint = area_times[10]
                 area_decile_from_midpoint = area_times[::2] - area_midpoint
                 rise_time = -area_decile_from_midpoint[1]
-
-                # Area fraction on top
-                peak.area_fraction_top =\
-                    np.sum(peak.area_per_channel[self.first_top_ch:self.last_top_ch + 1]) / peak.area
-                # rounding peak aft
-                if peak.area_fraction_top < 0:
-                    peak.area_fraction_top = 0
-                elif peak.area_fraction_top > 1:
-                    peak.area_fraction_top = 1
-
-                rise_time_cut = self.s1_rise_time_aft(peak.area_fraction_top)
-                # If area is large enough, change to a looser threshold
-                if peak.area > 100 or peak.area < 5:
-                    rise_time_cut = self.s1_rise_time_bound(peak.area)
-                coincidence = peak.n_contributing_channels
-                if rise_time < rise_time_cut or coincidence <= 4 or area_sum < 5:
+                # tight coincidence for peak classification
+                x = h['index_of_maximum']
+                l = peak.index_of_maximum - self.tight_coincidence_window
+                r = peak.index_of_maximum + self.tight_coincidence_window
+                tight_coincidence = len(np.unique(h['channel'][(x >= l) & (x <= r)]))
+                if (tight_coincidence >= 3 and rise_time < self.rise_time_threshold) or (len(h) <= 2 and area_sum < 50):
                     # Yes, this is an S1 candidate Or, this is a lone hit. Mark it as a peak,
                     # hits will be ignored in next stage.
                     s1_mask[l_i:r_i] = True
