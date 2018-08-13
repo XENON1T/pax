317f317f73fcddfd57668fc32ad41ead632c709b
Chris Tunnell <2406627+tunnell@users.noreply.github.com>
2018-08-13 09:19:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,7 @@
 import six
 import os
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
