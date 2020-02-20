004831bad9c87ba690b34d38d76a9c92a6e5db3d
Joey Howlett <jhowl01@gmail.com>
2020-02-20 14:38:43 -0600

    allow per-channel SPE-based classification

--- pax/plugins/peak_processing/ClassifyPeaks.py
+++ pax/plugins/peak_processing/ClassifyPeaks.py
@@ -1,4 +1,6 @@
-from pax import plugin, units
+from pax import plugin, units, utils, dsputils
+import pandas as pd
+import pdb
 
 
 class AdHocClassification1T(plugin.TransformPlugin):
@@ -7,12 +9,20 @@ class AdHocClassification1T(plugin.TransformPlugin):
         self.s1_rise_time_bound = self.config['s1_risetime_threshold']
         self.s1_width_bound = self.config['s1_width_threshold']
         self.tight_coincidence_threshold = self.config['tight_coincidence_threshold']
+        spe_dists = pd.read_csv(utils.data_file_name(self.config['photon_area_distribution']))
+        self.channel_area_thresholds = {}
+        for ch in spe_dists.columns[1:]:
+            mean_spe = (spe_dists['charge'] * spe_dists[ch]).sum() / spe_dists[ch].sum()
+            std_spe = ((spe_dists[ch] * ((spe_dists['charge'] - mean_spe)**2.0)).sum()/(spe_dists[ch].sum()))**0.5
+            self.channel_area_thresholds[int(ch)] = dsputils.adc_to_pe(self.config, int(ch)) * (mean_spe + (self.config['spe_cutoff_sigma'] * std_spe))
 
     def transform_event(self, event):
 
+
         for peak in event.peaks:
             # Don't work on noise and lone hit
-            if peak.type in ('noise', 'lone_hit'):
+            #if peak.type in ('noise', 'lone_hit'):
+            if peak.type in ('noise'):
                 continue
             # rounding peak aft, for future usage like BDT based classification
             if peak.area_fraction_top < 0:
@@ -24,7 +34,7 @@ class AdHocClassification1T(plugin.TransformPlugin):
             if -peak.area_decile_from_midpoint[1] < self.s1_rise_time_bound\
                     and peak.range_area_decile[9] < self.s1_width_bound:
                 # S1 requirements: Peak rises fast, and width (90p area) small
-                if peak.tight_coincidence >= self.tight_coincidence_threshold:
+                if (peak.largest_hit_area >= self.channel_area_thresholds[peak.largest_hit_channel]):
                     peak.type = 's1'
                 else:
                     # Too few PMTs contributing, hard to distinguish from junk
